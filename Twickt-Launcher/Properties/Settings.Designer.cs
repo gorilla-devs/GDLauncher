3e37e94b527bfa59e4f444ba13bd9f229e948ef5
Davide Ceschia <killpowa@users.noreply.github.com>
2017-12-20 14:37:34 +0100

    fixed version

--- Twickt-Launcher/Properties/Settings.Designer.cs
+++ Twickt-Launcher/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Twickt_Launcher.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -37,7 +37,7 @@ namespace Twickt_Launcher.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.4")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.5")]
         public string version {
             get {
                 return ((string)(this["version"]));
