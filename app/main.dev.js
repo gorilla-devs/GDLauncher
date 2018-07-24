d59a3d0fcc5c610296e5a7daa2b402b2c5a9d160
Davide Ceschia <davide.ceschia@gmail.com>
2018-07-24 17:17:39 +0200

    MacOS Fix close application

--- app/main.dev.js
+++ app/main.dev.js
@@ -49,9 +49,7 @@ const installExtensions = async () => {
 app.on('window-all-closed', () => {
   // Respect the OSX convention of having the application in memory even
   // after all windows have been closed
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 
