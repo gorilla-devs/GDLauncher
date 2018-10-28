635771d3d4512c8880c131fb5f43dedebcda8835
Davide Ceschia <davide.ceschia@gmail.com>
2018-10-28 14:38:48 +0100

    Added actions logging in prod too

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -2,6 +2,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history';
+import { createLogger } from 'redux-logger';
 import { routerMiddleware } from 'connected-react-router';
 import createRootReducer from '../reducers';
 
@@ -9,8 +10,22 @@ const history = createBrowserHistory({
   basename: window.location.pathname
 });
 const rootReducer = createRootReducer(history);
+
+const logger = createLogger({
+  // We need to hide the UPDATE_PROGRESS dispatches, since they are just too many and they slow down the execution
+  collapsed: true,
+  duration: true,
+  colors: {
+    title: () => '#8e44ad',
+    prevState: () => '#9E9E9E',
+    action: () => '#03A9F4',
+    nextState: () => '#4CAF50',
+    error: () => '#F20404'
+  }
+});
+
 const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
+const enhancer = applyMiddleware(thunk, router, logger);
 
 function configureStore(initialState?: counterStateType) {
   return createStore(rootReducer, initialState, enhancer);
