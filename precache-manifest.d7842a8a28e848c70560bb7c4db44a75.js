6f75ff20937da9d808732e52872e68596f87567c
Davide Ceschia <davide.ceschia@gmail.com>
2018-11-07 21:26:18 +0100

    New website

---
+++ precache-manifest.d7842a8a28e848c70560bb7c4db44a75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f183497231b4008f0a041b935aa0d38",
+    "url": "/static/media/logo.8f183497.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d26a009fa07f4eca70e8",
+    "url": "/static/js/main.d26a009f.chunk.js"
+  },
+  {
+    "revision": "3760344d3eb4f3473100",
+    "url": "/static/js/1.3760344d.chunk.js"
+  },
+  {
+    "revision": "d26a009fa07f4eca70e8",
+    "url": "/static/css/main.083e3683.chunk.css"
+  },
+  {
+    "revision": "3760344d3eb4f3473100",
+    "url": "/static/css/1.90cc00e8.chunk.css"
+  },
+  {
+    "revision": "b76dd33a97f603f1846dc283274e3874",
+    "url": "/index.html"
+  }
+];
