cd488fc0f790b6c5a9781932c85eb2d5b611f76c
Gianmarco <cavallogianmarco@gmail.com>
2018-12-30 20:16:50 +0100

    Basic ServerList implementation
    server list basic logic

--- app/components/ServerManager/ServerManager.js
+++ app/components/ServerManager/ServerManager.js
@@ -10,12 +10,28 @@ type Props = {};
 
 export default class ServerManager  extends Component<Props> {
   props: Props;
+  constructor(props){
+    super(props);
+    this.state={
+      serverName: ["peppe", "peppe", "peppe","peppe"]
+    };
+  }
+
 
   render() {
     return (
       <div className={styles.container}>
-        <div className={styles.serverSettings}></div>
-        <div className={styles.Serverlist}></div>
+        <div className={styles.serverSettings}>
+        
+        </div>
+        <div className={styles.Serverlist}>
+
+          {this.state.serverName.length > 0 &&
+            this.state.serverName.map(name => (
+            <div className={styles.server1}><h1>{name.value}</h1></div>
+          ))}
+
+        </div>
       </div>
     );
   }
