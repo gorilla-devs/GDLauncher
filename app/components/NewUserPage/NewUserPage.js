735a653d0ddecdb5604545669b3a182c00d50886
Davide Ceschia <davide.ceschia@gmail.com>
2019-02-18 23:36:10 +0100

    v0.11.1-beta

--- app/components/NewUserPage/NewUserPage.js
+++ app/components/NewUserPage/NewUserPage.js
@@ -8,8 +8,9 @@ import store from '../../localStore';
 import background from '../../assets/images/login_background.jpg';
 import circleBlue from '../../assets/images/circleBlue.svg';
 
-
-const colors = store.get('settings') ? store.get('settings').theme : THEMES.default;
+const colors = store.get('settings')
+  ? store.get('settings').theme
+  : THEMES.default;
 
 const defaultStyle = {
   transition: `all ${700}ms ease-in-out`,
@@ -18,51 +19,74 @@ const defaultStyle = {
   backgroundPosition: 'center center',
   backgroundRepeat: 'no-repeat',
   backgroundSize: '0 0'
-}
-
+};
 
 const internalDivStyles = {
   entered: { opacity: 1 }
-}
+};
 
 const NewUserPage = props => {
   const [mounted, setMounted] = useState(true);
   const transitionStyles = {
     entered: {
-      backgroundSize: `${window.screen.availWidth * 3}px ${window.screen.availHeight * 3}px`
+      backgroundSize: `${window.screen.availWidth * 3}px ${window.screen
+        .availHeight * 3}px`
     },
     exiting: {
       backgroundSize: `0px 0px`
     }
   };
   return (
-    <div style={{
-      background: `linear-gradient( ${colors['secondary-color-2']}8A, ${colors['secondary-color-2']}8A), url(${background})`
-    }}>
-      <Transition in={mounted} timeout={{ enter: 250, exit: 0 }} appear={true} key="1">
-        {(state) => (
-          <div style={{
-            ...defaultStyle,
-            ...transitionStyles[state],
-          }}>
-            <div style={{
-              opacity: 0,
-              transition: 'all 400ms ease-in-out',
-              padding: 30,
-              ...internalDivStyles[state]
-            }}>
+    <div
+      style={{
+        background: `linear-gradient( ${colors['secondary-color-2']}8A, ${
+          colors['secondary-color-2']
+        }8A), url(${background})`
+      }}
+    >
+      <Transition
+        in={mounted}
+        timeout={{ enter: 250, exit: 0 }}
+        appear
+        key="1"
+      >
+        {state => (
+          <div
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}
+          >
+            <div
+              style={{
+                opacity: 0,
+                transition: 'all 400ms ease-in-out',
+                padding: 30,
+                ...internalDivStyles[state]
+              }}
+            >
               <h1 style={{ fontSize: 37 }}>Welcome to GDLauncher!</h1>
               <div style={{ display: 'flex' }}>
                 <p style={{ fontSize: 25 }}>
                   Hello {props.username}!<br />
-                  GDLauncher is free and open source,
-                  it wouldn't exist without its community. If you find any bug or have any suggestion, tell us on Discord!<br /><br />
+                  GDLauncher is free and open source, it wouldn't exist without
+                  its community. If you find any bug or have any suggestion,
+                  tell us on Discord!
+                  <br />
+                  <br />
                   Happy gaming!
                 </p>
-                <iframe style={{
-                  marginTop: '-70px',
-                  paddingLeft: 30
-                }} src="https://discordapp.com/widget?id=398091532881756161&theme=dark" width="350" height="515" allowtransparency="true" frameborder="0"></iframe>
+                <iframe
+                  style={{
+                    marginTop: '-70px',
+                    paddingLeft: 30
+                  }}
+                  src="https://discordapp.com/widget?id=398091532881756161&theme=dark"
+                  width="350"
+                  height="515"
+                  allowtransparency="true"
+                  frameborder="0"
+                />
               </div>
               <DelayLink
                 to="home"
@@ -71,8 +95,9 @@ const NewUserPage = props => {
                 style={{
                   position: 'absolute',
                   bottom: 30,
-                  fontSize: 25,
-                }}>
+                  fontSize: 25
+                }}
+              >
                 GOT IT
               </DelayLink>
             </div>
@@ -80,11 +105,11 @@ const NewUserPage = props => {
         )}
       </Transition>
     </div>
-  )
+  );
 };
 
 const mapStateToProps = state => ({
   username: state.auth.displayName
-})
+});
 
 export default connect(mapStateToProps)(NewUserPage);
