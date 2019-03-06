33ecf04f2da7b125b5bb6efd912f20474061f825
Davide Ceschia <davide.ceschia@gmail.com>
2019-03-06 11:12:11 +0100

    Added flow types to cursemeta APIs

--- app/utils/cursemeta.js
+++ app/utils/cursemeta.js
@@ -1,61 +1,70 @@
-import React, { useState, useEffect } from 'react';
+// @flow
+
 import axios from 'axios';
 import log from 'electron-log';
 import { CURSEMETA_API_URL } from '../constants';
 
-const makeRequest = async (url, params = null) => {
+const makeRequest = async (url: string, params: {} = {}) => {
   const requestPayload = {
     params
   };
   try {
     const response = await axios.get(url, requestPayload);
-    console.log(response);
     return response.data;
   } catch (err) {
     log.error(err);
   }
 };
 
-export const getAddon = async addonID => {
+export const getAddon = async (addonID: number | string) => {
   const url = `${CURSEMETA_API_URL}/direct/addon/${addonID}`;
   return makeRequest(url);
 };
 
-export const getAddonFiles = async addonID => {
+export const getAddonFiles = async (addonID: number | string) => {
   const url = `${CURSEMETA_API_URL}/direct/addon/${addonID}/files`;
   return makeRequest(url);
 };
 
-export const getAddonDescription = async addonID => {
+export const getAddonDescription = async (addonID: number | string) => {
   const url = `${CURSEMETA_API_URL}/direct/addon/${addonID}/description`;
   return makeRequest(url);
 };
 
-export const getAddonFile = async (addonID, fileID) => {
+export const getAddonFile = async (
+  addonID: number | string,
+  fileID: number | string
+) => {
   const url = `${CURSEMETA_API_URL}/direct/addon/${addonID}/file/${fileID}`;
   return makeRequest(url);
 };
 
 export const getSearch = (
-  type, // Can be either 'mods' or 'modpacks'
-  searchFilter,
-  pageSize,
-  index,
-  sort,
-  isSortDescending,
-  gameVersion?
+  type: 'mods' | 'modpacks',
+  searchFilter: string,
+  pageSize: number,
+  index: number,
+  sort:
+    | 'Featured'
+    | 'Popularity'
+    | 'LastUpdated'
+    | 'Name'
+    | 'Author'
+    | 'TotalDownloads',
+  isSortDescending: boolean,
+  gameVersion?: string
 ) => {
   const url = `${CURSEMETA_API_URL}/direct/addon/search`;
-  const params = {
+  const params: {} = {
     gameId: 432,
     sectionId: type === 'mods' ? 6 : 4471,
     categoryId: 0,
     pageSize,
     sort,
     isSortDescending,
-    ...(gameVersion && gameVersion),
     index,
-    searchFilter
+    searchFilter,
+    ...(gameVersion && gameVersion)
   };
   return makeRequest(url, params);
 };
