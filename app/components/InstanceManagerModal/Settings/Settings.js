f1e8f288cc43798d83239ceb73ffa54f547e1d18
Gianmarco <cavallogianmarco@gmail.com>
2019-03-09 13:30:08 +0100

    Made functional settings component

--- app/components/InstanceManagerModal/Settings/Settings.js
+++ app/components/InstanceManagerModal/Settings/Settings.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { routerActions } from 'connected-react-router';
 import { bindActionCreators } from 'redux';
@@ -14,154 +14,154 @@ import { history } from '../../../store/configureStore';
 import ForgeManager from './ForgeManager';
 
 const FormItem = Form.Item;
-type Props = {};
 
 let watcher = null;
 
-class Instances extends Component<Props> {
-  props: Props;
+function Instances(props) {
+  const [instanceConfig, setInstanceConfig] = useState(null);
+  const [checkingForge, setCheckingForge] = useState(true);
+  const [unMounting, setUnMounting] = useState(false);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      instanceConfig: null,
-      checkingForge: true,
-      unMounting: false
-    };
-  }
-
-  componentDidMount = async () => {
+  async function ReadConfig(){
     try {
       let config = JSON.parse(
         await promisify(fs.readFile)(
-          path.join(PACKS_PATH, this.props.instance, 'config.json')
+          path.join(PACKS_PATH, props.instance, 'config.json')
         )
       );
-      this.setState({ instanceConfig: config });
+      setInstanceConfig(config);
       watcher = fs.watch(
-        path.join(PACKS_PATH, this.props.instance, 'config.json'),
+        path.join(PACKS_PATH, props.instance, 'config.json'),
         { encoding: 'utf8' },
         async (eventType, filename) => {
           config = JSON.parse(
             await promisify(fs.readFile)(
-              path.join(PACKS_PATH, this.props.instance, 'config.json')
+              path.join(PACKS_PATH, props.instance, 'config.json')
             )
           );
-          this.setState({ instanceConfig: config });
+          setInstanceConfig(config);
         }
       );
     } catch (err) {
       log.error(err.message);
     } finally {
-      this.setState({ checkingForge: false });
+      setCheckingForge(false);
     }
-  };
 
-  componentWillUnmount = () => {
-    watcher.close();
-  };
+  }
 
-  handleSubmit = e => {
+  useEffect(() => {
+    ReadConfig();
+    return () => {
+      watcher.close();
+    }
+  }, []);
+
+
+
+  function handleSubmit(e) {
     e.preventDefault();
-    this.props.form.validateFields(async (err, values) => {
+    props.form.validateFields(async (err, values) => {
       if (!err) {
         try {
           await promisify(fs.access)(path.join(PACKS_PATH, values.packName));
           message.warning('An instance with this name already exists.');
         } catch (err) {
-          const packFolder = path.join(PACKS_PATH, this.props.instance);
+          const packFolder = path.join(PACKS_PATH, props.instance);
           const newPackFolder = path.join(PACKS_PATH, values.packName);
           await promisify(fs.rename)(packFolder, newPackFolder);
-          this.props.close();
+          props.close();
         }
       }
     });
   };
 
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div>
-        <h2>Edit Instance Settings</h2>
-        <Form layout="inline" onSubmit={this.handleSubmit}>
-          <div>
-            <div
+  function OverrideArgs() {
+
+  }
+
+  const { getFieldDecorator } = props.form;
+  return (
+    <div>
+      <h2>Edit Instance Settings</h2>
+      <Form layout="inline" onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              height: '60px',
+              margin: 0
+            }}
+          >
+            <FormItem>
+              {getFieldDecorator('packName', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input a valid name',
+                    pattern: new RegExp(
+                      '^[a-zA-Z0-9_.-]+( [a-zA-Z0-9_.-]+)*$'
+                    )
+                  }
+                ],
+                initialValue: props.instance
+              })(
+                <Input
+                  size="large"
+                  style={{
+                    width: 300,
+                    display: 'inline-block',
+                    height: 60
+                  }}
+                  prefix={
+                    <Icon
+                      type="play-circle"
+                      theme="filled"
+                      style={{ color: 'rgba(255,255,255,.8)' }}
+                    />
+                  }
+                  placeholder="Instance Name"
+                />
+              )}
+            </FormItem>
+            <Button
+              icon="save"
+              size="large"
+              type="primary"
+              htmlType="submit"
               style={{
-                width: '100%',
-                display: 'flex',
-                height: '60px',
-                margin: 0
+                width: 150,
+                display: 'inline-block',
+                height: 60
               }}
             >
-              <FormItem>
-                {getFieldDecorator('packName', {
-                  rules: [
-                    {
-                      required: true,
-                      message: 'Please input a valid name',
-                      pattern: new RegExp(
-                        '^[a-zA-Z0-9_.-]+( [a-zA-Z0-9_.-]+)*$'
-                      )
-                    }
-                  ],
-                  initialValue: this.props.instance
-                })(
-                  <Input
-                    size="large"
-                    style={{
-                      width: 300,
-                      display: 'inline-block',
-                      height: 60
-                    }}
-                    prefix={
-                      <Icon
-                        type="play-circle"
-                        theme="filled"
-                        style={{ color: 'rgba(255,255,255,.8)' }}
-                      />
-                    }
-                    placeholder="Instance Name"
-                  />
-                )}
-              </FormItem>
-              <Button
-                icon="save"
-                size="large"
-                type="primary"
-                htmlType="submit"
-                style={{
-                  width: 150,
-                  display: 'inline-block',
-                  height: 60
-                }}
-              >
-                Rename
+              Rename
               </Button>
-            </div>
           </div>
-        </Form>
+        </div>
+      </Form>
 
-        <Card style={{ marginTop: 15 }} title="Forge Manager">
-          {!this.state.checkingForge ? (
-            <ForgeManager
-              name={this.props.instance}
-              data={this.state.instanceConfig}
-              closeModal={this.props.close}
-            />
-          ) : null}
-        </Card>
+      <Card style={{ marginTop: 15 }} title="Forge Manager">
+        {!checkingForge ? (
+          <ForgeManager
+            name={props.instance}
+            data={instanceConfig}
+            closeModal={props.close}
+          />
+        ) : null}
+      </Card>
 
-        <Card style={{ marginTop: 15, maxHeight: 150 }} title="Override global java arguments">
-          <div style={{ display: 'inline' }}>
-            <Input className={styles.JavaArginput}/>
-              <Button type="primary" >Set</Button>
-              <Switch className={styles.JavaArgswitch}></Switch>
-          </div>
-        </Card>
+      <Card style={{ marginTop: 15, maxHeight: 150 }} title="Override global java arguments">
+        <div style={{ display: 'inline' }}>
+          <Input className={styles.JavaArginput} />
+          <Button type="primary" >Set</Button>
+          <Switch className={styles.JavaArgswitch} onChange={() => OverrideArgs()} ></Switch>
+        </div>
+      </Card>
 
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
