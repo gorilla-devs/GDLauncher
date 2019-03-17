d929bab9ce56dcafaebf02c9701f69b5478b6b0c
Gianmarco <cavallogianmarco@gmail.com>
2019-03-17 16:44:24 +0100

    instancesManager made functional

--- app/components/InstanceManagerModal/Settings/Settings.js
+++ app/components/InstanceManagerModal/Settings/Settings.js
@@ -1,217 +1,216 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { routerActions } from 'connected-react-router';
 import { bindActionCreators } from 'redux';
-import { Form, Input, Icon, Button, message } from 'antd';
+import { Form, Input, Icon, Button, message, Slider } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUndo, faCheck } from '@fortawesome/free-solid-svg-icons';
 import path from 'path';
 import { promisify } from 'util';
-import fs from 'fs';
+import fs, { stat } from 'fs';
 import log from 'electron-log';
 import Card from '../../Common/Card/Card';
 import { PACKS_PATH } from '../../../constants';
 import styles from './Settings.scss';
+import JavaMemorySlider from './javaMemorySlider';
 import { history } from '../../../store/configureStore';
+import { setJavaMemory } from '../../../actions/settings';
 import ForgeManager from './ForgeManager';
 
 const FormItem = Form.Item;
-type Props = {};
 
-let watcher = null;
+function Instances(props) {
+  const [is64bit, setIs64bit] = useState(true);
+  const [instanceConfig, setInstanceConfig] = useState(null);
+  const [checkingForge, setCheckingForge] = useState(true);
+  const [unMounting, setUnMounting] = useState(false);
 
-class Instances extends Component<Props> {
-  props: Props;
+  let watcher = null;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      instanceConfig: null,
-      checkingForge: true,
-      unMounting: false
-    };
-  }
-
-  componentDidMount = async () => {
+  async function configManagment() {
     try {
       let config = JSON.parse(
         await promisify(fs.readFile)(
-          path.join(PACKS_PATH, this.props.instance, 'config.json')
+          path.join(PACKS_PATH, props.instance, 'config.json')
         )
       );
-      this.setState({ instanceConfig: config });
+      //this.setState({ instanceConfig: config });
+      setInstanceConfig(config);
       watcher = fs.watch(
-        path.join(PACKS_PATH, this.props.instance, 'config.json'),
+        path.join(PACKS_PATH, props.instance, 'config.json'),
         { encoding: 'utf8' },
         async (eventType, filename) => {
           config = JSON.parse(
             await promisify(fs.readFile)(
-              path.join(PACKS_PATH, this.props.instance, 'config.json')
+              path.join(PACKS_PATH, props.instance, 'config.json')
             )
           );
-          this.setState({ instanceConfig: config });
+          setInstanceConfig(config);
+          //this.setState({ instanceConfig: config });
         }
       );
     } catch (err) {
       log.error(err.message);
     } finally {
-      this.setState({ checkingForge: false });
+      setCheckingForge(false);
+      //this.setState({ checkingForge: false });
     }
-  };
+  }
 
-  componentWillUnmount = () => {
-    watcher.close();
-  };
+  useEffect(() => {
+    configManagment();
+    return () => {
+      watcher.close();
+    };
+  }, []);
 
-  handleSubmit = e => {
+  function handleSubmit(e) {
     e.preventDefault();
-    this.props.form.validateFields(async (err, values) => {
+    props.form.validateFields(async (err, values) => {
       if (!err) {
         try {
           await promisify(fs.access)(path.join(PACKS_PATH, values.packName));
           message.warning('An instance with this name already exists.');
         } catch (err) {
-          const packFolder = path.join(PACKS_PATH, this.props.instance);
+          const packFolder = path.join(PACKS_PATH, props.instance);
           const newPackFolder = path.join(PACKS_PATH, values.packName);
           await promisify(fs.rename)(packFolder, newPackFolder);
-          this.props.close();
+          props.close();
         }
       }
     });
-  };
+  }
 
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div>
-        <h2>Edit Instance Settings</h2>
-        <Form layout="inline" onSubmit={this.handleSubmit}>
-          <div>
-            <div
+  const { getFieldDecorator } = props.form;
+  return (
+    <div>
+      <h2>Edit Instance Settings</h2>
+      <Form layout="inline" onSubmit={e => handleSubmit(e)}>
+        <div>
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              height: '60px',
+              margin: 0
+            }}
+          >
+            <FormItem>
+              {getFieldDecorator('packName', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input a valid name',
+                    pattern: new RegExp('^[a-zA-Z0-9_.-]+( [a-zA-Z0-9_.-]+)*$')
+                  }
+                ],
+                initialValue: props.instance
+              })(
+                <Input
+                  size="large"
+                  style={{
+                    width: 300,
+                    display: 'inline-block',
+                    height: 60
+                  }}
+                  prefix={
+                    <Icon
+                      type="play-circle"
+                      theme="filled"
+                      style={{ color: 'rgba(255,255,255,.8)' }}
+                    />
+                  }
+                  placeholder="Instance Name"
+                />
+              )}
+            </FormItem>
+            <Button
+              icon="save"
+              size="large"
+              type="primary"
+              htmlType="submit"
               style={{
-                width: '100%',
-                display: 'flex',
-                height: '60px',
-                margin: 0
+                width: 150,
+                display: 'inline-block',
+                height: 60
               }}
             >
-              <FormItem>
-                {getFieldDecorator('packName', {
-                  rules: [
-                    {
-                      required: true,
-                      message: 'Please input a valid name',
-                      pattern: new RegExp(
-                        '^[a-zA-Z0-9_.-]+( [a-zA-Z0-9_.-]+)*$'
-                      )
-                    }
-                  ],
-                  initialValue: this.props.instance
-                })(
-                  <Input
-                    size="large"
-                    style={{
-                      width: 300,
-                      display: 'inline-block',
-                      height: 60
-                    }}
-                    prefix={
-                      <Icon
-                        type="play-circle"
-                        theme="filled"
-                        style={{ color: 'rgba(255,255,255,.8)' }}
-                      />
-                    }
-                    placeholder="Instance Name"
-                  />
-                )}
-              </FormItem>
-              <Button
-                icon="save"
-                size="large"
-                type="primary"
-                htmlType="submit"
-                style={{
-                  width: 150,
-                  display: 'inline-block',
-                  height: 60
-                }}
-              >
-                Rename
-              </Button>
-            </div>
+              Rename
+            </Button>
           </div>
-        </Form>
+        </div>
+      </Form>
 
-        <Card style={{ marginTop: 15 }} title="Forge Manager">
-          {!this.state.checkingForge ? (
-            <ForgeManager
-              name={this.props.instance}
-              data={this.state.instanceConfig}
-              closeModal={this.props.close}
-            />
-          ) : null}
-        </Card>
+      <Card style={{ marginTop: 15 }} title="Forge Manager">
+        {!checkingForge ? (
+          <ForgeManager
+            name={props.instance}
+            data={instanceConfig}
+            closeModal={props.close}
+          />
+        ) : null}
+      </Card>
 
-        <Card style={{ marginTop: 15, height: 200 }} title="Java Manager">
-          <div style={{ display: 'inline', verticalAlign: 'middle' }}>
-            <Input
-              style={{
-                display: 'inline-block',
-                maxWidth: '76%',
-                marginRight: '10px',
-                marginBottom: 10,
-                marginTop: 4,
-                backgroundColor: 'var(--secondary-color-1)'
-              }}
-            />
-            <Button.Group
+      <Card style={{ marginTop: 15, height: 200 }} title="Java Manager">
+        <JavaMemorySlider
+          ram={props.settings.java.memory}
+          is64bit={is64bit}
+          updateMemory={props.setJavaMemory}
+        />
+        <div style={{ display: 'inline', verticalAlign: 'middle' }}>
+          <Input
+            style={{
+              display: 'inline-block',
+              maxWidth: '76%',
+              marginRight: '10px',
+              marginBottom: 10,
+              marginTop: 4,
+              backgroundColor: 'var(--secondary-color-1)'
+            }}
+          />
+          <Button.Group
+            style={{
+              maxWidth: '60%',
+              marginBottom: 10,
+              marginTop: 4
+            }}
+          >
+            <Button
               style={{
                 maxWidth: '60%',
                 marginBottom: 10,
                 marginTop: 4
               }}
+              type="primary"
             >
-              <Button
-                style={{
-                  maxWidth: '60%',
-                  marginBottom: 10,
-                  marginTop: 4
-                }}
-                type="primary"
-              >
-                <FontAwesomeIcon icon={faCheck} />
-              </Button>
-              <Button
-                style={{
-                  maxWidth: '60%',
-                  marginBottom: 10,
-                  marginTop: 4
-                }}
-                type="primary"
-              >
-                <FontAwesomeIcon icon={faUndo} />
-              </Button>
-            </Button.Group>
-          </div>
-        </Card>
-      </div>
-    );
-  }
+              <FontAwesomeIcon icon={faCheck} />
+            </Button>
+            <Button
+              style={{
+                maxWidth: '60%',
+                marginBottom: 10,
+                marginTop: 4
+              }}
+              type="primary"
+            >
+              <FontAwesomeIcon icon={faUndo} />
+            </Button>
+          </Button.Group>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
-  return {};
+  return {
+    settings: state.settings
+  };
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      goBack: routerActions.goBack
-    },
-    dispatch
-  );
-}
+const mapDispatchToProps = {
+  setJavaMemory
+};
 
 export default Form.create()(
   connect(
