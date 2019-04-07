e7ebaf7210a0112ec2f5f4df944bc271a05e7691
Davide Ceschia <davide.ceschia@gmail.com>
2019-04-07 20:48:31 +0200

    Added support for Forge 1.13.2

---
+++ app/utils/forgeHelpers.js
@@ -0,0 +1,49 @@
+// @flow
+
+import axios from 'axios';
+import log from 'electron-log';
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+import { CURSEFORGE_MODLOADERS_API, META_PATH, INSTANCES_PATH } from '../constants';
+import { arraify } from './strings';
+
+const makeRequest = async (url: string, params: {} = {}) => {
+  const requestPayload = {
+    params
+  };
+  try {
+    const response = await axios.get(url, requestPayload);
+    return response.data;
+  } catch (err) {
+    log.error(err);
+  }
+};
+
+export const getForgeVersionJSON = async (forgeVersion: string) => {
+  const url = `https://addons-ecs.forgesvc.net/api/minecraft/modloader/forge-${forgeVersion}`;
+  return makeRequest(url);
+};
+
+export const checkForgeMeta = async (forgeVersion: string) => {
+  return JSON.parse(
+    await promisify(fs.readFile)(
+      path.join(
+        META_PATH,
+        'net.minecraftforge',
+        `forge-${forgeVersion}`,
+        `forge-${forgeVersion}.json`
+      )
+    )
+  )
+}
+
+export const checkForgeDownloaded = async (forgePath: string) => {
+  return promisify(fs.access)(
+    path.join(
+      INSTANCES_PATH,
+      'libraries',
+      ...arraify(forgePath)
+    )
+  )
+}
