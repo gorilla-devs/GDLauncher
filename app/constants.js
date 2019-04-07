57306e70eb6cf4f4d79e309405c03f073d676b5c
AppleTheGolden <scotsbox@protonmail.com>
2019-04-07 21:05:43 +0200

    I may have forgotten to import app

--- app/constants.js
+++ app/constants.js
@@ -2,6 +2,7 @@ import path from 'path';
 import electron from 'electron';
 import { platform, homedir } from 'os';
 import getAppPath from './utils/getAppPath';
+import os from 'os';
 
 export const WINDOWS = 'win32';
 export const LINUX = 'linux';
