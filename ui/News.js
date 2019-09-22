49003287bfae84c9817911c6a2c6671443820989
Ladvace <cavallogianmarco@gmail.com>
2019-09-22 17:37:51 +0200

    almost finished news carousel fixed

--- ui/News.js
+++ ui/News.js
@@ -1,40 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContentLoader from 'react-content-loader';
 import styled from 'styled-components';
+import { shell } from 'electron';
 
 const Carousel = styled.div`
   width: 788px;
   height: 100%;
+  overflow: hidden;
   border-radious: 2px;
   ...props.style;
 `;
-
-const ImageSlider = styled.ul`
+// transform: translate(${props => -788 * (props.imageIndex + 1)})
+const ImageSlider = styled.div`
   display: flex;
+  flex-direction: row;
+  overflow: hidden;
   justify-content: space-between;
-  position: absolute;
   padding: 0;
   margin: 0;
-  top: 0;
+  margin: 0 auto 0 auto;
+  width: 1000%;
   height: 100%;
+  z-index: 0;
+  transform: translate(${props => -788 * (props.imageIndex + 1)});
+`;
+
+const Slide = styled.div`
+  display: inline-block;
+  position: relative;
+  top: 0;
+  height: 158px%;
   width: 100%;
+  margin0 2px 0 2px;
   background-size: cover;
   background-position: center;
   z-index: 0;
 `;
 
-const Slide = styled.li`
-  display: inline;
-  position: absolute;
-  top: 0;
-  height: 100%;
-  width: 100%;
-  margin0 2px 0 2px;
-  z-index: 0;
-`;
-
 const ImageSlide = styled.img`
   position: absolute;
+  display: block;
   top: 0;
   height: 100%;
   width: 100%;
@@ -65,7 +70,7 @@ const Select = styled.div`
   bottom: 15px;
   padding: 0;
   margin: 0;
-  width: 158px;
+  width: 200px;
   height: 5px;
   left: 50%;
   margin-left: -79px;
@@ -117,25 +122,52 @@ type Props = {
   description: string
 };
 
+// function openNews(inf) {
+//   console.log(inf.url);
+// }
+
 function ImageList(props) {
+  const currentImageIndex = props.currentImageIndex;
   const news = props.news;
   const listImages = news.map((inf, i) => (
-    <Slide>
+    <Slide key={i}>
       <Title>{inf.title}</Title>
       <SubTitle>{inf.description}</SubTitle>
       <Gradient />
-      <ImageSlide key={i} image={inf.image} />
+      <ImageSlide image={inf.image} />
     </Slide>
   ));
 
-  return <ImageSlider>{listImages}</ImageSlider>;
+  return <ImageSlider imageIndex={props.imageIndex}>{listImages}</ImageSlider>;
 }
+
+function SelectNews(props) {
+  const news = props.news;
+  const setCurrentImageIndex = props.setCurrentImageIndex;
+  const selectElementList = news.map((inf, i) => (
+    <SelectElement key={i} onClick={() => setCurrentImageIndex(i)} />
+  ));
+
+  return <Select>{selectElementList}</Select>;
+}
+
 function News(props: Props) {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(null);
+  useEffect(() => {
+    console.log(currentImageIndex);
+  }, [currentImageIndex]);
 
   return (
     <Carousel style={props.style}>
-      <Select>
+      <SelectNews
+        news={props.news}
+        setCurrentImageIndex={setCurrentImageIndex}
+        currentImageIndex={currentImageIndex}
+      />
+      {/* <Select>
+        <SelectElement />
+        <SelectElement />
+        <SelectElement />
         <SelectElement />
         <SelectElement />
         <SelectElement />
@@ -145,7 +177,8 @@ function News(props: Props) {
         <SelectElement />
         <SelectElement />
       </Select>
-      <ImageList news={props.news} />
+       */}
+      <ImageList news={props.news} imageIndex={currentImageIndex} />
     </Carousel>
   );
 }
