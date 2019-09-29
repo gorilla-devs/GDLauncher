af588858b56f1f665120b16ef3705e8a9323930c
Ladvace <cavallogianmarco@gmail.com>
2019-09-29 13:48:25 +0200

    modpacks search bar

--- app/components-new/views/modpacks/index.js
+++ app/components-new/views/modpacks/index.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import Input from '../../../../ui/Input';
+import styled from 'styled-components';
+
+const SortingNavbar = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  top: 0px;
+  height: 41px;
+  width: 100%;
+  margin: 0 auto;
+`;
 
 export default () => {
-  return <div>modpacks</div>;
+  return (
+    <div style={{ backgroundColor: 'transparent', height: '100%' }}>
+      <SortingNavbar>
+        <div
+          style={{
+            width: '50%',
+            height: '41px',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginRight: 'auto',
+            marginLeft: 'auto'
+          }}
+        >
+          <div>
+            <Input placeholder="search"></Input>
+          </div>
+          <div style={{ width: '150px' }}>
+            sort by &nbsp;
+            <Input />
+          </div>
+        </div>
+      </SortingNavbar>
+    </div>
+  );
 };
---
+++ ui/Input.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: ${props => props.width || '90px'};
+  height: ${props => props.width || '30px'};;
+  padding 10px;
+  background: transparent;
+  border-radius: 5px;
+  border: 2px solid #0f7173;
+  color: #e1e2e4;
+  font-family: Glacial Indifference;
+  font-size: 14px;
+  line-height: 16px;
+  margin: 0 !important;
+
+  &:hover {
+    transition: all 0.1s ease-in-out;
+    background: #212b36;
+    opacity: 10%;
+    border: 2px solid #0f7173;
+  }
+
+  &:active {
+    transition: all 0.1s ease-in-out;
+    background: #212b36;
+    opacity: 10%;
+    border: 2px solid #0f7173;
+  }
+  ...props.style;
+`;
+
+type Props = {
+  placeholder: string
+  //   children: React.node
+};
+
+function GDInput(props: Props) {
+  return (
+    <Input
+      name={props.name}
+      type={props.type}
+      placeholder={props.placeholder}
+      width={props.width}
+      height={props.height}
+      style={props.style}
+    />
+  );
+}
+
+export default GDInput;
