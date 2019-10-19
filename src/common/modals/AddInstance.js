ea0746f174599cac292968f766b3a3a963e870e9
Ladvace <cavallogianmarco@gmail.com>
2019-10-19 22:19:06 +0200

    Vanilla versions list

--- src/common/modals/AddInstance.js
+++ src/common/modals/AddInstance.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import Button from "@material-ui/core/Button";
+import Select from "@material-ui/core/Select";
+import CheckBox from "@material-ui/core/CheckBox";
+// import { CheckBox } from "../../ui";
 import Modal from "../components/Modal";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const AddInstance = () => {
+  const [snapshot, setSnapshot] = useState(false);
+  const [version, setVersion] = useState("");
+
+  const vanillaManifest = useSelector(state => state.app.vanillaManifest);
+  console.log(vanillaManifest);
   return (
     <Modal
       css={`
@@ -11,7 +21,72 @@ const AddInstance = () => {
         max-width: 1500px;
       `}
     >
-      <Button>Test</Button>
+      <div
+        css={`
+          width: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        `}
+      >
+        <div
+          css={`
+            height: 100px;
+            width: 100px;
+          `}
+        >
+          Vanilla version
+          <Select
+            css={`
+              margin-left: 25px;
+            `}
+            value={version}
+            onChange={e => setVersion(e.target.value)}
+          >
+            {/* {vanillaManifest.versions
+          .filter(versions =>
+            // versions => versions.type === "release"
+            snapshot ? versions.type === "snapshot" : true
+          )
+          .map((version, i) => (
+            <MenuItem key={version.id} value={i}>
+              {version.id}
+            </MenuItem>
+          ))} */}
+
+            {snapshot
+              ? vanillaManifest.versions.map((version, i) => (
+                  <MenuItem key={version.id} value={i}>
+                    {version.id}
+                  </MenuItem>
+                ))
+              : vanillaManifest.versions
+                  .filter(versions => versions.type === "release")
+                  .map((version, i) => (
+                    <MenuItem key={version.id} value={i}>
+                      {version.id}
+                    </MenuItem>
+                  ))}
+          </Select>
+          <CheckBox
+            css={`
+              position: relative;
+              top: 20px;
+              margin-left: 25px;
+            `}
+            onClick={() => setSnapshot(!snapshot)}
+          />
+          <div
+            css={`
+              position: relative;
+              top: 25px;
+              margin-left: 15px;
+            `}
+          >
+            Snapshots
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 };
