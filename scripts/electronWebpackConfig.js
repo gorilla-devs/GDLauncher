bbe2209c7997c7de3de7d698fc7c7fdc04233240
Davide <davide.ceschia@gmail.com>
2020-02-10 23:35:43 +0100

    Reduced executable size from 400mb to 170mb

---
+++ scripts/electronWebpackConfig.js
@@ -0,0 +1,108 @@
+/**
+ * Webpack config for production electron main process
+ */
+
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const TerserPlugin = require("terser-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+const baseConfig = {
+  externals: [],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, "..", "build"),
+    // https://github.com/webpack/webpack/issues/1114
+    libraryTarget: "commonjs2"
+  },
+
+  /**
+   * Determine the array of extensions that should be used to resolve modules.
+   */
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    modules: [path.join(__dirname, "..", "build"), "node_modules"]
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "production"
+    }),
+
+    new webpack.NamedModulesPlugin()
+  ]
+};
+
+module.exports = merge.smart(baseConfig, {
+  devtool: "nosources-source-map",
+
+  mode: "production",
+
+  target: "electron-main",
+
+  entry: "./public/electron.js",
+
+  output: {
+    path: path.join(__dirname, ".."),
+    filename: "./build/electron.js"
+  },
+
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        sourceMap: false,
+        cache: true
+      })
+    ]
+  },
+
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode:
+        process.env.OPEN_ANALYZER === "true" ? "server" : "disabled",
+      openAnalyzer: process.env.OPEN_ANALYZER === "true"
+    }),
+
+    /**
+     * Create global constants which can be configured at compile time.
+     *
+     * Useful for allowing different behaviour between development builds and
+     * release builds
+     *
+     * NODE_ENV should be production so that modules do not perform certain
+     * development checks
+     */
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "production",
+      DEBUG_PROD: false,
+      START_MINIMIZED: false
+    })
+  ],
+
+  /**
+   * Disables webpack processing of __dirname and __filename.
+   * If you run the bundle in node.js it falls back to these values of node.js.
+   * https://github.com/webpack/webpack/issues/2010
+   */
+  node: {
+    __dirname: false,
+    __filename: false
+  }
+});
