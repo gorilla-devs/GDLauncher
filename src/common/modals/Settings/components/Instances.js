798926b495f2349f4bb6f1f4c4d76a8b6200b4e9
Gianmarco <31326058+Ladvace@users.noreply.github.com>
2020-02-22 23:10:58 +0100

    Settings Style fix (#333)
    * Settings Style fix
    
    * fix
    
    Co-authored-by: Davide Ceschia <10165264+killpowa@users.noreply.github.com>

--- src/common/modals/Settings/components/Instances.js
+++ src/common/modals/Settings/components/Instances.js
@@ -16,19 +16,30 @@ import { updateInstancesPath } from "../../../reducers/settings/actions";
 
 const Instances = styled.div`
   width: 100%;
-  height: 500px;
 `;
 
 const AutodetectPath = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
   margin-top: 38px;
-  margin-bottom: 20px;
   width: 100%;
-  height: 120px;
+  height: 100px;
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
 
 const OverridePath = styled.div`
   width: 100%;
   height: 70px;
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
 
 const InstanceCustomPath = styled.div`
@@ -37,7 +48,6 @@ const InstanceCustomPath = styled.div`
 `;
 
 const Title = styled.h3`
-  position: absolute;
   font-size: 15px;
   font-weight: 700;
   color: ${props => props.theme.palette.text.primary};
@@ -58,7 +68,9 @@ const MainTitle = styled.h1`
   color: ${props => props.theme.palette.text.primary};
 `;
 
-const StyledButtons = styled(Button)``;
+const StyledButtons = styled(Button)`
+  float: right;
+`;
 
 async function clearSharedData(InstancesPath, setDeletingInstances) {
   setDeletingInstances(true);
@@ -94,7 +106,7 @@ export default function MyAccountPreferences() {
       <MainTitle
         css={`
           float: left;
-          margin: 0;
+          margin: 30px 0 20px 0;
         `}
       >
         Instances
@@ -102,75 +114,70 @@ export default function MyAccountPreferences() {
       <AutodetectPath>
         <Title
           css={`
-            position: absolute;
-            top: 80px;
+            width: 300px;
+            float: left;
+            margin: 0 0 10px 0;
           `}
         >
           Clear Shared Data&nbsp; <FontAwesomeIcon icon={faTrash} />
         </Title>
-        <Paragraph
-          css={`
-            position: absolute;
-            top: 100px;
-          `}
-        >
-          Deletes all the shared files between instances. Doing this will result
-          in the complete loss of the instances data
-        </Paragraph>
-        {deletingInstances ? (
-          <StyledButtons
-            onClick={() => clearSharedData(InstancesPath, setDeletingInstances)}
-            disabled
-            css={`
-              margin-top: 70px;
-              float: right;
-            `}
-            color="primary"
-          >
-            Clear
-          </StyledButtons>
-        ) : (
-          <StyledButtons
-            onClick={() => clearSharedData(InstancesPath, setDeletingInstances)}
-            css={`
-              margin-top: 70px;
-              float: right;
-            `}
-            color="primary"
-          >
-            Clear
-          </StyledButtons>
-        )}
+        <div>
+          <Paragraph css="">
+            Deletes all the shared files between instances. Doing this will
+            result in the complete loss of the instances data
+          </Paragraph>
+          {deletingInstances ? (
+            <StyledButtons
+              onClick={() =>
+                clearSharedData(InstancesPath, setDeletingInstances)
+              }
+              disabled
+              css={`
+                float: right;
+              `}
+              color="primary"
+            >
+              Clear
+            </StyledButtons>
+          ) : (
+            <StyledButtons
+              onClick={() =>
+                clearSharedData(InstancesPath, setDeletingInstances)
+              }
+              css={`
+                float: right;
+              `}
+              color="primary"
+            >
+              Clear
+            </StyledButtons>
+          )}
+        </div>
       </AutodetectPath>
       <Hr />
+      <Title
+        css={`
+          width: 250px;
+        `}
+      >
+        Override Default Instance Path&nbsp; <FontAwesomeIcon icon={faFolder} />
+      </Title>
       <OverridePath>
-        <Title
-          css={`
-            position: absolute;
-            top: 180px;
-          `}
-        >
-          Override Default Instance Path&nbsp;{" "}
-          <FontAwesomeIcon icon={faFolder} />
-        </Title>
-        <Paragraph
-          css={`
-            position: absolute;
-            top: 200px;
-          `}
-        >
-          If enabled, instances will be downloaded in the selected path you need
-          to restart the launcher for this settings to applay
-        </Paragraph>
-        <Switch
-          style={{
-            float: "right",
-            marginTop: "20px"
-          }}
-          color="primary"
-          onChange={setOverrideInstancesPath}
-          checked={overrideInstancesPath}
-        />
+        <div>
+          <Paragraph>
+            If enabled, instances will be downloaded in the selected path you
+            need to restart the launcher for this settings to applay
+          </Paragraph>
+          <Switch
+            style={{
+              float: "right",
+              marginTop: "20px"
+            }}
+            color="primary"
+            onChange={setOverrideInstancesPath}
+            checked={overrideInstancesPath}
+          />
+        </div>
       </OverridePath>
       {overrideInstancesPath && (
         <InstanceCustomPath>
