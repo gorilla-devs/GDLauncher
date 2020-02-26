b54dfe97440d6505c049e752cfbce69d77d3e6f7
Ladvace <cavallogianmarco@gmail.com>
2020-02-26 21:20:53 +0100

    stuff

--- src/common/modals/InstanceManager/ScreenShot.js
+++ src/common/modals/InstanceManager/ScreenShot.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React, { useEffect, useState } from "react";
-import { promises as fs } from "fs";
+import { promises as fs, watch } from "fs";
 import { clipboard } from "electron";
 import fse from "fs-extra";
 import path from "path";
@@ -10,7 +10,7 @@ import { useSelector } from "react-redux";
 import request from "request";
 import { ContextMenuTrigger, ContextMenu, MenuItem } from "react-contextmenu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faCopy, faLink } from "@fortawesome/free-solid-svg-icons";
 import { _getInstancesPath } from "../../utils/selectors";
 import _ from "lodash";
 import axios from "axios";
@@ -26,7 +26,7 @@ const Container = styled.div`
 `;
 
 const Bar = styled.div`
-  height: 50px;
+  min-height: 40px;
   width: 100%;
   background: ${props => props.theme.palette.secondary.main};
 `;
@@ -40,6 +40,26 @@ const DateSection = styled.div`
   margin: 10px 0 10px 0;
   &&:first-child {
     margin-top: 0;
+  }
+`;
+
+const DeleteButton = styled(FontAwesomeIcon)`
+  margin-left: 10px;
+  transition: color 0.3s ease-in-out;
+  &&:hover {
+    path {
+      color: ${props => props.theme.palette.colors.red};
+    }
+  }
+`;
+
+const DeletAllButton = styled(MenuItem)`
+  && {
+    background: ${props => props.theme.palette.colors.red};
+  }
+  &&:hover {
+    background: ${props => props.theme.palette.colors.red};
+    filter: brightness(80%);
   }
 `;
 
@@ -68,10 +88,35 @@ const Photo = styled.img`
   }
 `;
 
-const deleteFile = async (InstancePath, instanceName, fileName) => {
-  await fse.remove(
-    path.join(InstancePath, instanceName, "screenshots", fileName)
-  );
+const deleteFile = async (
+  InstancePath,
+  instanceName,
+  fileName,
+  multipleOrNot,
+  selectedScreens,
+  setSelectedScreens
+) => {
+  console.log("LOLL11", selectedScreens);
+  if (!multipleOrNot) {
+    await fse.remove(
+      path.join(InstancePath, instanceName, "screenshots", fileName)
+    );
+    setSelectedScreens([
+      selectedScreens.splice(selectedScreens.indexOf(fileName), 1)
+    ]);
+  } else {
+    Promise.all(
+      selectedScreens.map(async screenShot => {
+        await fse.remove(
+          path.join(InstancePath, instanceName, "screenshots", screenShot)
+        );
+        setSelectedScreens([
+          selectedScreens.splice(selectedScreens.indexOf(fileName), 1)
+        ]);
+      })
+    );
+  }
+  console.log("LOLL", selectedScreens);
 };
 
 const calcDate = async ScreenShotsDir => {
@@ -111,34 +156,22 @@ const imgurShare = async image => {
   let screenShot = await fs.readFile(image);
 
   let b64Img = screenShot.toString("base64");
-  // let data = Buffer.from(str, "base64");
 
-  // console.log("data", b64Img);
+  var bodyFormData = new FormData();
+  bodyFormData.append("image", b64Img);
 
-  var options = {
-    method: "POST",
-    url: "https://api.imgur.com/3/image",
+  const res = await axios.post("https://api.imgur.com/3/image", bodyFormData, {
     headers: {
       Authorization: `Client-ID ${clientId}`
-    },
-    formData: {
-      image:
-        "iVBORw0KGgoAAAANSUhEUgAAANIAAAAzCAYAAADigVZlAAAQN0lEQVR4nO2dCXQTxxnHl0LT5jVteHlN+5q+JCKBJITLmHIfKzBHHCCYBAiEw+I2GIMhDQ0kqQolIRc1SV5e+prmqX3JawgQDL64bK8x2Ajb2Bg7NuBjjSXftmRZhyXZ1nZG1eL1eGa1kg2iyua9X2TvzvHNN/Ofb2Z2ZSiO4ygZGZm+EXADZGSCgYAbICMTDATcABmZYCDgBsjIBAMBN0BGJhgIuAEyMsGA1wQdHZ1UV1cX5XK5qM7OzgcMRuNTrSbTEraq6strhdfzruTk5Wpz8q5c1l7Jyb6szc3K1l7RggtFxcWX2dvVB02mtmVOp3NIV2fnQFie2WyB5QS84TIy/YnXBFBI8BMM/pDqat0XzIVM08lTSVxyytn6jAuZV4FuzmtzclJz8/LT8vML0nJzr54HYkpLS88oTkxMMZ48mchlXrxUX1ffcBCUM8xms8lCkgk6pCT6aZvZvCrzYpbu2PfxHAg8l+obGmOt1vaJQBAPkvI5nM5fWyyWWTU1tfuA+IqOHDvGgehVCK4pA91oGZn+xluCAc0thtj4hCT72XOp9S0thi2FBQWPvb13z9RN61QH5s8NYxbMDct7KXyudt7MGeeWLFrwn8iVKz7auDZy3Z7dbzz91p43B8ZsjYLlDKmprd3/ffwpLjWNqbW32xcFuuEyMv2J2M1BJpMpKiExxZKZeamira1tvvqdt8OWL1l8asq4kNbRzz7NTRo7uuMPo4Y7Rz/zFBc64lluzHNDuZFDFe5PICx25/aY2B3bogf/dd9fKCA+CuytohOSkjuyLmtLXRwXGujGy8j0F8Qbdrt9bDpzQQ8jSHl5+dLt0VsOThgzwj7i6Se5kOHDuIljR9mXRrykjZj/wlVeSONHP8+FhykrJoeOsY8aNoQLAYJa9erShIPvvRsKhQTK/YleX3Pw5KlErpKt+iLQjZeR6S9IN35VXl75r3gw4HU6/Z6ojes/gMKAUQiKBQKiUvvLC1/MXL18WcKsaZOrJ4WObly7euUJsOQ7FjZ9Sh2IVC4oLhihZk6d1LB5/dpt+9R/hnuq4Xl5VwvT0jLKXS7XOHgaCAm0I2Rk+gL2os1mewXsiUw5uXlZn8T9LVI5ZWI1jEQTxozkgECgkDrmKqfrFy8ILwJ7om+3bNoQumTRwtDoqE0fTBsf2ggwg+jVBdOCT7eYwGfnti2bQXA6ME2nr9mbnHLOWV/fEI3WTdO0jMzdZjBAKWBwX8ojCqm8vOJoYvLp9qPfHTmy5rXlJ+BSbtzI5+5EI4ALRCTHHHpaQ8zWqOidO2IooBAKRKRDQDwGevJ4w8SQUR0e0bmB0QxEKh2IYsdbTW0zmIxM4/Wi4q9BfQMkCikCoAEUADgEeI3xOOVedkicp14e1V2uLwSpTwxNAPwRaGC7OQFqQp9xGDT+1ksUUubFrMoLFy/VL5g7+4ep48fa+P0Pz9jnn4H7JCcQBbP79V1rgJDmASE9um7NqvmxMdFbVateiwd7KKswHx+dwBKwzGq1jgDRrjQ7W5sB6hvsRUhQQCyh8Sg4xwW64/oTpUQ/CIm7xz652yg9flb40R+xIn5i/LWJKKSk5NOuwqIi7cSQkXooAD6ywE8YneDyLWrDuq/WR67+BvxcB5dtG9dGHgF7oZsgSuWFz555c0LISKcwIvHlAHSdnR0P37h5699pzIW6NrNlptFoIglJ7cOAgcTf40711nH3g5AguEH3/4YGaZPSj/6Ix/hGmKd/hXQqIanz5q1b8WA5VwOXdLwgoIjAsk2/Y1v0odUrXj0OT+vgNSCkjgXzZleANF3wpI6PRALxcDDt7BlTby+NWPgdqOPBisrKz8E+zFFXX79Sp9fjhKQiDAqjx6kRHmfCdHDWZek+zCp+gnac6i7XhxOSUkAExiZI7D32y73wtbKfy/CnPDdEISUkJjsrKiqPhocp86ZPGGeDSzkIWJa1Rq5ccXyDas1X8PBBuG9Cow8UE/yEaYYPeZybPnFcM1gGRh/6+KNhNbV1o7Mua29dysrOdblcQ4SvDHmMg5s/I2ZAxNP+bQz5zaVaABz0ij7kh6D7NVJnwL1NLJLXn47DCQmXjkXSqAnpFB4/CO2KkODjEE861B9i7VcKwPldgaQJQfKi4yFWkNZbPXzZuP4iQRobaLrBIhEpubP0xq2E9989MHnLpg3rX5hFlz3/1BMcWLaVRm/eeIieNL4KRhi450EjDxQOvAf2T+mrli9bDZaAq3Zu37b3nbf2zvnwg/d/DoRENbcYRmhzcn84n5peDkQ0FbNHUmMGjD/LtsGesnCi5GEEnYbLH+clP9ox6ABiRdKzmDz9ISR0wKgx7WJE7ILtxUUxlQQfGDFtQutC7cH1OUPIi8NbPWjZUtBgbIzApFMQhZSccrbrav61zAqWfWR79JbJ8+eG5Q97/HccfB0I/P4eEJADRigoJP6NBvgzBC715s2coTuwf9+0qI3rKbB3ooCQKCAkCgiJgkKCS7uWFuMbiUkpjpzcvCvg9yGIkFicwZiGeRMR7oQPB+x8VEy+5OcRDiDcoCdBErI/QsINdmH5pGiPAxUT6cQLxYjkY5D7aozdaiQNQ8iLoz+EhPY1i7FRg7ORKKTUtHSdVptTarPZhr737oFHgRj+7lmeVcRsjfrwxdkzc+DSDj50VU6Z0LR5/drDK5a8HLt4QfhusAfaBUQz8tDHHw/atE5FEhLkods6/ZfHjsdzZWXlJwRCGoxppAbTKG+gjeadoyZ0Duo43MbU6LmuJpTPCwk3WGFHqTyg9xiJbcIJSS2AtJkWG9R89Imgew8mI91zmcfQPfeo/D21iC9wdUZg2oaWoaG7xYvm59vFQ6qHt0EloQycb4WTN25cuttBFBKIRpfAsstkNpvD4Xtye9/802PLFi/6J1y6LXpx3mUQleJARHKCaGRbvWLZO1AwQEgUEBIFhOQWDRAS5UVIFOfinrheVHw2MTmFEwgJ1yAVxvFiKDBlaJA0uJmbrycEcw+3P0PTCDtOeJ1F8uKWCFL2fr5EOZzNOL+g0Qq9Lxz0IQQ7ceUKhSR2jzRxqb2Uj/MP46Ueb2WwyH1hREaPzln+HlFIjY1N+1NSzlirq/Wfg99/9saunVRszLaHdu3YHg32PueAOP4Klm8lk0JHt4GfZ6yPXE0tf2WxZCHZ7Q7K4XC667I77IuZC5nehIRzvBhqJD86s/KgM7CG7p4FUafh8pPsRAeFhu69SfWnjTgBisEi5aKDoQBjl7f9FSqgWBq/FPdVSIxIvTh/+Sok3OSI5kf7XbgvR/1yR2REIXV0dIRmX9beys7WljsdzhEeIQFBxFDLXl5E7doRMzFs+pTG+XNmFX726acPHo6Loz45fJhasmihG29CstraqfZ2+wCXyzWCZau+T0w63d9CQgcy6aACdRxDcJqKkJ9kp9Q9iK9tVGPyqQXgDkbg7wqCX6SgRmyAdmpo7w/JAyEk1Calj2WgYjOKXL8zsRKFBKNQA4hKp8+c62poaPwjfI0HLOfcX4WAYoqO2jQKLPVSdr++azsUkK9CagdCstnah14rvJ767XdHHSUlN64IhISbOdDO9IZYp4gNTIbGd7wCk1ch0jHodf4VJjGkHDig9nKYNLCDWSQN/3YD6hdWgl38JOLtpA9FTEg4f6JlqwX3pAoJTRMiUgZDKAP1HcyHTrgaYR4xIVFOp/PJgmuFFfngf52dnU+Q0nkDLuOsVitlb293Cwhib7dTFotlWloaU3s1vyANpHsUObVDHcISGt1XIWkIzpXSabhlli8zsD+oJdpGirRS/YIDd4LJeurCTX68WKQsqXA+E9qG+ho9FSSVIbwnVUgajB1olO8xEYgKCdLaaoouKv6hrNXYOt9ut8PlGAF3hMGWAa83NjVRNpDG4XDcwWg0rklLZ7iS0hufgXQDESHhliBCx3oDdUYBIR1LqAOtGxct0DqEHYd7eHg3hMRKbD9D8KvUZ3MqTFuFbVKI+AIdwDh/4soXTj5ouxkabyfJBl+E5G0f2isfUUjwD5RAzGbzQzW1dXOqdbphNbW1VE0NHp1OD6KOTVRI7UCIgusP6Gtq9iWnnOmqul0dhXkgi3M+BM5+pNOtELp7pvDWMRDcC4x8B6OzLzrgcLOssOPQAcuK2N0XIfXqVI9tqJB5+8Xa7Eu96IuwuP4Suyf0J85ejhYX0t2MSBTBHh4Vmp4opJYWgxujsZWqr2+ggJAoXY2eAoO/F/Ce1YYXkVBIMKKB5SJc0sGl3rC8/ALt2fNpzQ6HM9zVW0i4WVXoRP5ZjprufrbB0d0RBfccx0h3v8aCK1voWLTjOE+d/GsxJEeLzbAFdPdRMv/KUSwtfX+Es4ulex42kHzGd74Cc8/ouc8LXen5PV6QD62XEaRXENrrbVI00uIPvMWExHl8F0/37DeSDb4KieRHFpeeKCSDwegGCqmurt4tFn9E1CMigaWd52/jQX5fUlqakprOmMB/LzU3N+OEJNYgKc735agYfbPBl6f/pI5jfMgnNVr5UiYPuqxV+5CXFz4uAguFgFuKS53hSQj7UuzrD3x09LYXQ9vN0GQ/k8aOGpe+T0K6XV1NWaxWKYcNA1sMhgdANHLvgzo7u9zXK1n20PnzaVYQ8ZbB5SFBSPzszkp0vgLjEG+dyNL4iEBacvBovHQcFIeU42ZWpEP7KiTSS75qifmF/sS1lwc30H3pB1xkEgpJIZKfj5q4yOevkEjix054fgsJfu0BwkcZEqCs3zQ2Ne8pLin5urpad8hkaltQUnLjGbDfimQyLhjg298gDe7tb9Isoabx3wRV0/jXTvgBrfKkE+aLE8kjzCtcQvD5FB7UCLgyQgh288tTJSEfaVJB68QRQXt/N1GBaRuPmsY/OyP5UYov+DTCvBq65/JRCGq/AlM3tF+4xBSzQYncw7VPCOlhff8ICQqotq7OfRghWKphMZstaxKTUywnTp5qPHP2vOn0mXNcKpNhPpWYxKWmpjeDZd0WtG4vjZORuRcoafEI2QO/hASXdAajUcozpEGF14uPpgPhWK22xRaLdUbV7eo3b9ws28+yVXsdDvtceHonC0nmPoShey89ien9jkjNLQaqrc1MxASw2donpaZn1JeVlyeBfdEv2232O/sjMe4DJ8r8+GDo7i8K4va1KrH8PgsJPkuC+yL4tgL8JAGPucvKK2MzM7PaWltbl4AyB/wvj10Wksz9CCeCaDSC+CQkGInq6utF90Q8oIzf5l0tuFheXvkPsI962HN6JwtJ5n6FofEiwn3hsxeShVQF9kVQRPDfSZKwN6Kampt3Xiu83mQymcL5a/BrE1BMspBk7kNUdO8TVeGJoCiShOR+DaiuTvKfFQbpHqmoqMzW6/WJ8PgbOQ6XkQlKsBd5IUFaDAbJkQhitdpWgKUg226zLYS/y0KS+TGAvdjc3OKmqamFamtroywWq+gpHY/ZbBnU3GL4FHx+A8r5BeEhrYxM0BFwA2RkgoGAGyAjEwwE3AAZmWAg4AbIyAQDATdARiYYCLgBMjLBQMANkJEJBgJugIxMMPBfChd6NRZ5pkMAAAAASUVORK5CYII="
     }
-  };
+  });
 
-  // request(options, function(error, response) {
-  //   if (error) throw new Error(error);
-  //   console.log(response.body);
-  // });
-
-  axios(options)
-    .then(function(response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+  if (res.status == 200) {
+    console.log(res.data.data.link);
+    clipboard.writeText(res.data.data.link);
+  } else {
+    console.error;
+  }
 };
 
 const calcDateTitle = days => {
@@ -151,13 +184,17 @@ const calcDateTitle = days => {
   else if (parsedDays >= 365) return `${Math.floor(days / 365)} years ago`;
 };
 
-const startListener = async ScreenShotsDir => {
-  fs.watch(ScreenShotsDir, (event, filename) => {
+const startListener = (ScreenShotsDir, selectedScreens, setSelectedScreens) => {
+  watch(ScreenShotsDir, (event, filename) => {
+    console.log("piripicchio");
     if (filename) {
+      updateState(ScreenShotsDir, selectedScreens, setSelectedScreens);
       console.log(`${filename} file Changed`);
     }
   });
 };
+
+const updateState = (ScreenShotsDir, selectedScreens, setSelectedScreens) => {};
 
 const selectAll = async (
   setSelectedScreens,
@@ -183,7 +220,7 @@ const ScreenShot = ({ instanceName }) => {
     calcDate(ScreenShotsDir).then(sortedScreens => {
       setGroupedStortedPhoto(_.groupBy(sortedScreens, "days"));
     });
-    // startListener(ScreenShotsDir);
+    startListener(ScreenShotsDir, selectedScreens, setSelectedScreens);
 
     // listener.on("error", async () => {
     //   // Check if the folder exists and create it if it doesn't
@@ -200,6 +237,7 @@ const ScreenShot = ({ instanceName }) => {
       css={`
         display: flex;
         flex-direction: column;
+        width: 100%;
       `}
     >
       <Bar>
@@ -208,8 +246,22 @@ const ScreenShot = ({ instanceName }) => {
             selectAll(setSelectedScreens, ScreenShotsDir, selectedScreens)
           }
           css={`
-            vertical-align: middle;
+            margin: 7px 20px;
           `}
+        />
+
+        <DeleteButton
+          onClick={() =>
+            deleteFile(
+              InstancePath,
+              instanceName,
+              "",
+              true,
+              selectedScreens,
+              setSelectedScreens
+            )
+          }
+          icon={faTrash}
         />
       </Bar>
       <Container>
@@ -264,9 +316,34 @@ const ScreenShot = ({ instanceName }) => {
                         setIsHovered(false);
                       }}
                     >
+                      {selectedScreens.length > 1 ? (
+                        <DeletAllButton
+                          onClick={() =>
+                            deleteFile(
+                              InstancePath,
+                              instanceName,
+                              file.name,
+                              false,
+                              selectedScreens,
+                              setSelectedScreens
+                            )
+                          }
+                        >
+                          Delete All&nbsp;
+                          <FontAwesomeIcon icon={faTrash} />
+                        </DeletAllButton>
+                      ) : null}
+
                       <MenuItem
                         onClick={() =>
-                          deleteFile(InstancePath, instanceName, file.name)
+                          deleteFile(
+                            InstancePath,
+                            instanceName,
+                            file.name,
+                            false,
+                            selectedScreens,
+                            setSelectedScreens
+                          )
                         }
                       >
                         Delete&nbsp;
@@ -279,14 +356,16 @@ const ScreenShot = ({ instanceName }) => {
                           );
                         }}
                       >
-                        Copy the image
+                        Copy the image&nbsp;
+                        <FontAwesomeIcon icon={faCopy} />
                       </MenuItem>
                       <MenuItem
                         onClick={() => {
                           imgurShare(path.join(ScreenShotsDir, file.name));
                         }}
                       >
-                        Copy the image via url
+                        Share the image via url&nbsp;
+                        <FontAwesomeIcon icon={faLink} />
                       </MenuItem>
                     </ContextMenu>
                   </span>
