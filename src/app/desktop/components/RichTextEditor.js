dd2150db82e6e6331f3ccf1a760f715b5baad98e
DylanHameleers <41838333+DylanHameleers@users.noreply.github.com>
2020-03-24 17:37:36 +0100

    Add files via upload
    k

--- src/app/desktop/components/RichTextEditor.js
+++ src/app/desktop/components/RichTextEditor.js
@@ -14,7 +14,7 @@ const Overview = () => {
       <Options>
         <div>Bold</div>
       </Options>
-      <textarea placeholder="Type your notes here!" />
+      <textarea placeholder="Type your notes here!!" />
     </>
   );
 };
