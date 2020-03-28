bbe994a07bc14cb376a3d89e589c81aff296291d
Gianmarco <cavallogianmarco@gmail.com>
2020-03-28 23:17:31 +0100

    java checker animation

--- src/common/components/ModalsManager.js
+++ src/common/components/ModalsManager.js
@@ -86,7 +86,19 @@ const ModalContainer = ({ unmounting, children, preventClose }) => {
 
   const back = e => {
     e.stopPropagation();
-    if (preventClose) return;
+
+    if (preventClose) {
+      setModalStyle({
+        animation: `modalShake 0.25s linear infinite`
+      });
+
+      setTimeout(() => {
+        setModalStyle({
+          transform: 'scale(1)'
+        });
+      }, 500);
+      return;
+    }
     dispatch(closeModal());
   };
 
@@ -117,7 +129,49 @@ const ModalContainer = ({ unmounting, children, preventClose }) => {
 
   return (
     <Overlay onClick={back} style={bgStyle}>
-      <Modal style={modalStyle} onClick={back}>
+      <Modal
+        // css={`
+        //   @keyframes shake {
+        //     0% {
+        //       transform: translate(1px, 1px) rotate(0deg);
+        //     }
+        //     10% {
+        //       transform: translate(-1px, -2px) rotate(-1deg);
+        //     }
+        //     20% {
+        //       transform: translate(-3px, 0px) rotate(1deg);
+        //     }
+        //     30% {
+        //       transform: translate(3px, 2px) rotate(0deg);
+        //     }
+        //     40% {
+        //       transform: translate(1px, -1px) rotate(1deg);
+        //     }
+        //     50% {
+        //       transform: translate(-1px, 2px) rotate(-1deg);
+        //     }
+        //     60% {
+        //       transform: translate(-3px, 1px) rotate(0deg);
+        //     }
+        //     70% {
+        //       transform: translate(3px, 1px) rotate(-1deg);
+        //     }
+        //     80% {
+        //       transform: translate(-1px, -1px) rotate(1deg);
+        //     }
+        //     90% {
+        //       transform: translate(1px, 2px) rotate(0deg);
+        //     }
+        //     100% {
+        //       transform: translate(1px, -2px) rotate(-1deg);
+        //     }
+        //   }
+
+        //   animation: shake 0.3s linear infinite;
+        // `}
+        style={modalStyle}
+        onClick={back}
+      >
         {children}
       </Modal>
     </Overlay>
