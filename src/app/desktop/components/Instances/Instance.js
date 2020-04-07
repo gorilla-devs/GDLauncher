b8470e7b0be95439c1c55ad9d67b7578f203ddf1
Gianmarco <31326058+Ladvace@users.noreply.github.com>
2020-04-07 14:15:18 +0200

    playedTimeConversion (#376)
    * converter
    
    * f
    
    * linting
    
    Co-authored-by: Davide Ceschia <10165264+killpowa@users.noreply.github.com>

--- src/app/desktop/components/Instances/Instance.js
+++ src/app/desktop/components/Instances/Instance.js
@@ -144,6 +144,28 @@ const Instance = ({ instanceName }) => {
 
   const isPlaying = startedInstances[instanceName];
 
+  const calcTime = minutes => {
+    const days = Math.floor(minutes / 1440); // 60*24
+    const hours = Math.floor((minutes - days * 1440) / 60);
+    const min = Math.round(minutes % 60);
+    const weeks = Math.floor(days / 7);
+    const months = Math.floor(weeks / 4);
+
+    if (days < 7) {
+      if (days > 0) {
+        return `${days} d, ${hours} h, ${min} m`;
+      }
+      if (hours > 0) {
+        return `${hours} h, ${min} m`;
+      }
+      return `${min} minutes`;
+    }
+    if (months > 0) {
+      return `${months} months`;
+    }
+    return `${weeks} weeks`;
+  };
+
   useEffect(() => {
     if (instance.background) {
       fs.readFile(path.join(instancesPath, instanceName, instance.background))
@@ -193,7 +215,8 @@ const Instance = ({ instanceName }) => {
                   margin-right: 5px;
                 `}
               />
-              {instance.timePlayed} minutes
+
+              {calcTime(instance.timePlayed)}
             </TimePlayed>
             <MCVersion>{(instance.modloader || [])[1]}</MCVersion>
             {instanceName}
