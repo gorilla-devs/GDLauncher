c4d61fd1e4b6c04edd99a7e16c561b893d54136a
Davide <davide.ceschia@gmail.com>
2020-05-02 20:29:46 +0200

    Added more packaging for linux

--- scripts/createDeploy.js
+++ scripts/createDeploy.js
@@ -156,7 +156,17 @@ const commonConfig = {
     }
   },
   ...((!process.env.RELEASE_TESTING || process.platform === 'linux') && {
-    linux: type === 'setup' ? ['appimage:x64', 'zip:x64'] : []
+    linux:
+      type === 'setup'
+        ? [
+            'appimage:x64',
+            'zip:x64',
+            'deb:x64',
+            'rpm:x64',
+            'snap:x64',
+            'pacman:x64'
+          ]
+        : []
   }),
   ...((!process.env.RELEASE_TESTING || process.platform === 'win32') && {
     win: [type === 'setup' ? 'nsis-web:x64' : 'zip:x64']
