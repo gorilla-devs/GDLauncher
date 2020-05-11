b39b6fb86d04938db99cfff8a5f2424dbf1fef69
Davide <davide.ceschia@gmail.com>
2020-05-11 13:11:38 +0200

    Memoize component

--- src/app/desktop/views/Home.js
+++ src/app/desktop/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
@@ -112,4 +112,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default memo(Home);
