890a5d934496bee15d3958faac6ee9f2ac3ab8e7
Davide <davide.ceschia@gmail.com>
2020-05-20 13:48:07 +0200

    minmax on mac when double clicking navbar

--- public/electron.js
+++ public/electron.js
@@ -238,9 +238,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on('before-quit', async () => {
