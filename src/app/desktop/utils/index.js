4b239f6469b755f545c36a418a0d32dfa6204c31
Gianmarco <31326058+Ladvace@users.noreply.github.com>
2020-06-27 12:12:54 +0200

    mcversion changer (#504)
    * version changer
    
    * some fix
    
    * change modloader
    
    * some problem on fabric
    
    * finished
    
    * mcVersionChanger Modal
    
    * some code improvement
    
    * removed some useless code
    
    * filteredVersions moved to utils
    
    * filteredVersions to getFilteredVersions (name changed)
    
    * card component
    
    Co-authored-by: Davide Ceschia <10165264+killpowa@users.noreply.github.com>

--- src/app/desktop/utils/index.js
+++ src/app/desktop/utils/index.js
@@ -15,7 +15,11 @@ import {
   FABRIC,
   FORGE
 } from '../../../common/utils/constants';
-import { removeDuplicates } from '../../../common/utils';
+
+import {
+  removeDuplicates,
+  sortByForgeVersionDesc
+} from '../../../common/utils';
 import { getAddonFile, mcGetPlayerSkin } from '../../../common/api';
 import { downloadFile } from './downloader';
 
@@ -154,6 +158,111 @@ export const librariesMapper = (libraries, librariesPath) => {
       }, []),
     'url'
   );
+};
+
+export const getFilteredVersions = (
+  vanillaManifest,
+  forgeManifest,
+  fabricManifest
+) => {
+  const snapshots = vanillaManifest.versions
+    .filter(v => v.type === 'snapshot')
+    .map(v => v.id);
+  const versions = [
+    {
+      value: 'vanilla',
+      label: 'Vanilla',
+      children: [
+        {
+          value: 'release',
+          label: 'Releases',
+          children: vanillaManifest.versions
+            .filter(v => v.type === 'release')
+            .map(v => ({
+              value: v.id,
+              label: v.id
+            }))
+        },
+        {
+          value: 'snapshot',
+          label: 'Snapshots',
+          children: vanillaManifest.versions
+            .filter(v => v.type === 'snapshot')
+            .map(v => ({
+              value: v.id,
+              label: v.id
+            }))
+        },
+        {
+          value: 'old_beta',
+          label: 'Old Beta',
+          children: vanillaManifest.versions
+            .filter(v => v.type === 'old_beta')
+            .map(v => ({
+              value: v.id,
+              label: v.id
+            }))
+        },
+        {
+          value: 'old_alpha',
+          label: 'Old Alpha',
+          children: vanillaManifest.versions
+            .filter(v => v.type === 'old_alpha')
+            .map(v => ({
+              value: v.id,
+              label: v.id
+            }))
+        }
+      ]
+    },
+    {
+      value: 'forge',
+      label: 'Forge',
+      children: Object.entries(forgeManifest).map(([k, v]) => ({
+        value: k,
+        label: k,
+        children: v.sort(sortByForgeVersionDesc).map(child => ({
+          value: child,
+          label: child.split('-')[1]
+        }))
+      }))
+    },
+    {
+      value: 'fabric',
+      label: 'Fabric',
+      children: [
+        {
+          value: 'release',
+          label: 'Releases',
+          children: fabricManifest.mappings
+            .filter(v => !snapshots.includes(v.gameVersion))
+            .map(v => ({
+              value: v.version,
+              label: v.version,
+              children: fabricManifest.loader.map(c => ({
+                value: c.version,
+                label: c.version
+              }))
+            }))
+        },
+        {
+          value: 'snapshot',
+          label: 'Snapshots',
+          children: fabricManifest.mappings
+            .filter(v => snapshots.includes(v.gameVersion))
+            .map(v => ({
+              value: v.version,
+              label: v.version,
+              children: fabricManifest.loader.map(c => ({
+                value: c.version,
+                label: c.version
+              }))
+            }))
+        }
+      ]
+    }
+  ];
+  return versions;
 };
 
 export const isLatestJavaDownloaded = async (meta, userData, retry) => {
---
+++ src/common/modals/McVersionChanger.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Cascader } from 'antd';
+import styled from 'styled-components';
+import fss from 'fs-extra';
+import { useSelector, useDispatch } from 'react-redux';
+import path from 'path';
+import Modal from '../components/Modal';
+import { addToQueue } from '../reducers/actions';
+import { _getInstancesPath, _getInstance } from '../utils/selectors';
+import { closeModal } from '../reducers/modals/actions';
+import { FABRIC, VANILLA, FORGE } from '../utils/constants';
+import { getFilteredVersions } from '../../app/desktop/utils';
+
+const McVersionChanger = ({ instanceName }) => {
+  const vanillaManifest = useSelector(state => state.app.vanillaManifest);
+  const fabricManifest = useSelector(state => state.app.fabricManifest);
+  const forgeManifest = useSelector(state => state.app.forgeManifest);
+  const config = useSelector(state => _getInstance(state)(instanceName));
+  const instancePath = useSelector(state =>
+    path.join(_getInstancesPath(state), instanceName)
+  );
+
+  const dispatch = useDispatch();
+
+  const filteredVers = getFilteredVersions(
+    vanillaManifest,
+    forgeManifest,
+    fabricManifest
+  );
+
+  return (
+    <Modal
+      title="Minecraft Version Changer"
+      css={`
+        height: 380px;
+        width: 600px;
+      `}
+    >
+      <Container>
+        <Cascader
+          options={filteredVers}
+          onChange={async v => {
+            const isVanilla = v[0] === VANILLA;
+            const isFabric = v[0] === FABRIC;
+            const isForge = v[0] === FORGE;
+            if (isVanilla) {
+              dispatch(addToQueue(instanceName, [v[0], v[2]]));
+            } else if (isForge) {
+              try {
+                const manifest = await fss.readJson(
+                  path.join(instancePath, 'manifest.json')
+                );
+
+                dispatch(
+                  addToQueue(
+                    instanceName,
+                    v,
+                    manifest,
+                    `background${path.extname(config?.background)}`
+                  )
+                );
+              } catch (e) {
+                console.error(e);
+                dispatch(addToQueue(instanceName, v));
+              }
+            } else if (isFabric) {
+              const mappedItem = fabricManifest.mappings.find(
+                ver => ver.version === v[2]
+              );
+              const splitItem = v[2].split(mappedItem.separator);
+              dispatch(
+                addToQueue(instanceName, ['fabric', splitItem[0], v[2], v[3]])
+              );
+            }
+
+            dispatch(closeModal());
+            setTimeout(() => dispatch(closeModal()), 200);
+          }}
+          placeholder="Select a version"
+          size="large"
+          css={`
+            width: 400px;
+          `}
+        />
+      </Container>
+    </Modal>
+  );
+};
+
+export default McVersionChanger;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
