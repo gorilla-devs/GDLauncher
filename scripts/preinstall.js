f505ce42fdceb09b5e55db602f0299f6afa512d6
Davide <davide.ceschia@gmail.com>
2021-01-02 01:29:46 +0100

    15.5.0

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,4 +1,4 @@
-const REQUIRED_NODE_VERSION = 'v14.14.0';
+const REQUIRED_NODE_VERSION = 'v15.5.0';
 
 if (process.version !== REQUIRED_NODE_VERSION) {
   throw new Error(`Please install node ${REQUIRED_NODE_VERSION} x64`);
