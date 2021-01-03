ddbefb1d5dc479fac6bb71c1d1a16949f2596e0a
Davide <davide.ceschia@gmail.com>
2021-01-03 22:14:44 +0100

    check architecture

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,5 +1,7 @@
 const REQUIRED_NODE_VERSION = 'v15.5.0';
+const REQUIRED_NODE_ARCH = 'x64';
+const NODE_ARCH_VALID = process.arch === REQUIRED_NODE_ARCH;
 
-if (process.version !== REQUIRED_NODE_VERSION) {
+if (process.version !== REQUIRED_NODE_VERSION || !NODE_ARCH_VALID) {
   throw new Error(`Please install node ${REQUIRED_NODE_VERSION} x64`);
 }
