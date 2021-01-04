bde84620eadae7a8f1d8cb11e981441ae500feb7
Davide <davide.ceschia@gmail.com>
2021-01-04 03:29:11 +0100

    test

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,4 +1,4 @@
-const REQUIRED_NODE_VERSION = 'v15.5.0';
+const REQUIRED_NODE_VERSION = 'v14.14.0';
 const REQUIRED_NODE_ARCH = 'x64';
 const NODE_ARCH_VALID = process.arch === REQUIRED_NODE_ARCH;
 
