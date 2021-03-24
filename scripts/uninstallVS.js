1370c040e2f56524e6aa768f3bc36a3aa1bbcc42
Davide <davide.ceschia@gmail.com>
2021-03-24 11:42:38 +0100

    electron@9

--- scripts/uninstallVS.js
+++
@@ -1,7 +0,0 @@
-const { spawnSync } = require('child_process');
-
-spawnSync('vs_installer.exe', ['/uninstall', '--passive'], {
-  shell: true,
-  stdio: [process.stdio, process.stdio, process.stderr],
-  cwd: 'C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer'
-});
