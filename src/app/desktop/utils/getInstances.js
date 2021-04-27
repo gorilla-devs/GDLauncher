f75828c59e449dd4e01b3204276fe99ad2de8932
Gianmarco <31326058+Ladvace@users.noreply.github.com>
2021-04-27 19:42:04 +0200

    modloader to loader convertion fix (add the source) (#878)

--- src/app/desktop/utils/getInstances.js
+++ src/app/desktop/utils/getInstances.js
@@ -2,6 +2,7 @@ import path from 'path';
 import fse from 'fs-extra';
 import pMap from 'p-map';
 import { getDirectories } from '.';
+import { CURSEFORGE } from '../../../common/utils/constants';
 
 const getInstances = async instancesPath => {
   const mapFolderToInstance = async instance => {
@@ -19,8 +20,8 @@ const getInstances = async instancesPath => {
           loaderType,
           mcVersion,
           loaderVersion,
-          fileId,
-          addonId,
+          fileID,
+          projectID,
           source
         ] = config.modloader;
 
@@ -30,9 +31,9 @@ const getInstances = async instancesPath => {
             loaderType,
             mcVersion,
             ...(loaderVersion && { loaderVersion }),
-            ...(fileId && { fileID: fileId }),
-            ...(addonId && { projectID: addonId }),
-            ...(source && { source })
+            ...(fileID && { fileID }),
+            ...(projectID && { projectID }),
+            ...(!source && fileID && projectID && { source: CURSEFORGE })
           }
         };
 
