07fb3e3cbab27d86b1f8865165817a806f142007
Gianmarco <cavallogianmarco@gmail.com>
2021-07-23 14:12:01 +0200

    do not downloaded if already installed

--- src/common/modals/JavaSetup.js
+++ src/common/modals/JavaSetup.js
@@ -13,7 +13,11 @@ import { exec } from 'child_process';
 import { promisify } from 'util';
 import Modal from '../components/Modal';
 import { downloadFile } from '../../app/desktop/utils/downloader';
-import { convertOSToJavaFormat, get7zPath } from '../../app/desktop/utils';
+import {
+  convertOSToJavaFormat,
+  get7zPath,
+  isLatestJavaDownloaded
+} from '../../app/desktop/utils';
 import { _getTempPath } from '../utils/selectors';
 import { closeModal } from '../reducers/modals/actions';
 import { updateJava16Path, updateJavaPath } from '../reducers/settings/actions';
@@ -239,6 +243,21 @@ const AutomaticSetup = () => {
     const javaOs = convertOSToJavaFormat(process.platform);
     const javaMeta = javaManifest.find(v => v.os === javaOs);
     const java16Meta = java16Manifest.find(v => v.os === javaOs);
+    const manifests = {
+      java16: java16Manifest,
+      java: javaManifest
+    };
+
+    const isJava8Downloaded = await isLatestJavaDownloaded(
+      manifests,
+      userData,
+      true
+    );
+    const isJava16Downloaded = await isLatestJavaDownloaded(
+      manifests,
+      userData,
+      true
+    );
     const {
       version_data: { openjdk_version: version },
       binary_link: url,
@@ -256,80 +275,85 @@ const AutomaticSetup = () => {
     const downloadLocation = path.join(tempFolder, path.basename(url));
     const downloadjava16Location = path.join(tempFolder, path.basename(url16));
 
-    setCurrentStep('Java8 - Downloading');
-    await downloadFile(downloadLocation, url, p => {
-      ipcRenderer.invoke('update-progress-bar', parseInt(p, 10) / 100);
-      setDownloadPercentage(parseInt(p, 10));
-      setCurrentStepPercentage(p / (100 / 27.5), 10);
-    });
-    setCurrentStep('Java16 - Downloading');
-    await downloadFile(downloadjava16Location, url16, p => {
-      ipcRenderer.invoke('update-progress-bar', parseInt(p, 10) / 100);
-      setDownloadPercentage(parseInt(p, 10));
-      setCurrentStepPercentage(27.5 + p / (100 / 27.5), 10);
-    });
-
+    if (!isJava8Downloaded) {
+      setCurrentStep('Java8 - Downloading');
+      await downloadFile(downloadLocation, url, p => {
+        ipcRenderer.invoke('update-progress-bar', parseInt(p, 10) / 100);
+        setDownloadPercentage(parseInt(p, 10));
+        setCurrentStepPercentage(p / (100 / 27.5), 10);
+      });
+    }
+    if (!isJava16Downloaded) {
+      setCurrentStep('Java16 - Downloading');
+      await downloadFile(downloadjava16Location, url16, p => {
+        ipcRenderer.invoke('update-progress-bar', parseInt(p, 10) / 100);
+        setDownloadPercentage(parseInt(p, 10));
+        setCurrentStepPercentage(27.5 + p / (100 / 27.5), 10);
+      });
+    }
     ipcRenderer.invoke('update-progress-bar', -1);
     setDownloadPercentage(null);
     await new Promise(resolve => setTimeout(resolve, 500));
 
     const totalSteps = process.platform !== 'win32' ? 2 : 1;
-
-    setCurrentStep(`Java8 - Extracting 1 / ${totalSteps}`);
     const sevenZipPath = await get7zPath();
-    const firstExtraction = extractFull(downloadLocation, tempFolder, {
-      $bin: sevenZipPath,
-      $progress: true
-    });
-    await new Promise((resolve, reject) => {
-      firstExtraction.on('progress', ({ percent }) => {
-        ipcRenderer.invoke('update-progress-bar', percent);
-        setDownloadPercentage(percent);
-        setCurrentStepPercentage(
-          27.5 + 27,
-          5 + percent / (100 / totalSteps === 2 ? 10 : 20)
-        );
-      });
-      firstExtraction.on('end', () => {
-        resolve();
-      });
-      firstExtraction.on('error', err => {
-        reject(err);
-      });
-    });
-
-    await fse.remove(downloadLocation);
-
-    setCurrentStep(`Java16 - Extracting 1 / ${totalSteps}`);
-    const firstExtractionJava16 = extractFull(
-      downloadjava16Location,
-      tempFolder,
-      {
+    if (!isJava8Downloaded) {
+      setCurrentStep(`Java8 - Extracting 1 / ${totalSteps}`);
+      const firstExtraction = extractFull(downloadLocation, tempFolder, {
         $bin: sevenZipPath,
         $progress: true
-      }
-    );
+      });
+      await new Promise((resolve, reject) => {
+        firstExtraction.on('progress', ({ percent }) => {
+          ipcRenderer.invoke('update-progress-bar', percent);
+          setDownloadPercentage(percent);
+          setCurrentStepPercentage(
+            27.5 + 27,
+            5 + percent / (100 / totalSteps === 2 ? 10 : 20)
+          );
+        });
+        firstExtraction.on('end', () => {
+          resolve();
+        });
+        firstExtraction.on('error', err => {
+          reject(err);
+        });
+      });
 
-    console.log('sevenZip', sevenZipPath);
+      await fse.remove(downloadLocation);
+    }
+    if (!isJava16Downloaded) {
+      setCurrentStep(`Java16 - Extracting 1 / ${totalSteps}`);
+      const firstExtractionJava16 = extractFull(
+        downloadjava16Location,
+        tempFolder,
+        {
+          $bin: sevenZipPath,
+          $progress: true
+        }
+      );
 
-    await new Promise((resolve, reject) => {
-      firstExtractionJava16.on('progress', ({ percent }) => {
-        ipcRenderer.invoke('update-progress-bar', percent);
-        setDownloadPercentage(percent);
-        setCurrentStepPercentage(
-          27.5 +
+      console.log('sevenZip', sevenZipPath);
+
+      await new Promise((resolve, reject) => {
+        firstExtractionJava16.on('progress', ({ percent }) => {
+          ipcRenderer.invoke('update-progress-bar', percent);
+          setDownloadPercentage(percent);
+          setCurrentStepPercentage(
             27.5 +
-            (totalSteps === 2 ? 10 : 27.5) +
-            percent / (100 / (totalSteps === 2 ? 10 : 20))
-        );
+              27.5 +
+              (totalSteps === 2 ? 10 : 27.5) +
+              percent / (100 / (totalSteps === 2 ? 10 : 20))
+          );
+        });
+        firstExtractionJava16.on('end', () => {
+          resolve();
+        });
+        firstExtractionJava16.on('error', err => {
+          reject(err);
+        });
       });
-      firstExtractionJava16.on('end', () => {
-        resolve();
-      });
-      firstExtractionJava16.on('error', err => {
-        reject(err);
-      });
-    });
+    }
 
     await fse.remove(downloadjava16Location);
 
@@ -338,61 +362,64 @@ const AutomaticSetup = () => {
       ipcRenderer.invoke('update-progress-bar', -1);
       setDownloadPercentage(null);
       await new Promise(resolve => setTimeout(resolve, 500));
-      setCurrentStep(`Java8 - Extracting 2 / ${totalSteps}`);
+      if (!isJava8Downloaded) {
+        setCurrentStep(`Java8 - Extracting 2 / ${totalSteps}`);
 
-      const tempTarName = path.join(
-        tempFolder,
-        path.basename(url).replace('.tar.gz', '.tar')
-      );
+        const tempTarName = path.join(
+          tempFolder,
+          path.basename(url).replace('.tar.gz', '.tar')
+        );
 
-      const secondExtraction = extractFull(tempTarName, tempFolder, {
-        $bin: sevenZipPath,
-        $progress: true
-      });
-      await new Promise((resolve, reject) => {
-        secondExtraction.on('progress', ({ percent }) => {
-          ipcRenderer.invoke('update-progress-bar', percent);
-          setDownloadPercentage(percent);
-          setCurrentStepPercentage(
-            27.5 + 27.5 + 10 + 10 + percent / (100 / 10)
-          );
+        const secondExtraction = extractFull(tempTarName, tempFolder, {
+          $bin: sevenZipPath,
+          $progress: true
         });
-        secondExtraction.on('end', () => {
-          resolve();
+        await new Promise((resolve, reject) => {
+          secondExtraction.on('progress', ({ percent }) => {
+            ipcRenderer.invoke('update-progress-bar', percent);
+            setDownloadPercentage(percent);
+            setCurrentStepPercentage(
+              27.5 + 27.5 + 10 + 10 + percent / (100 / 10)
+            );
+          });
+          secondExtraction.on('end', () => {
+            resolve();
+          });
+          secondExtraction.on('error', err => {
+            reject(err);
+          });
         });
-        secondExtraction.on('error', err => {
-          reject(err);
-        });
-      });
 
-      await fse.remove(tempTarName);
+        await fse.remove(tempTarName);
+      }
+      if (!isJava16Downloaded) {
+        setCurrentStep(`Java16 - Extracting 2 / ${totalSteps}`);
+        const tempTarName16 = path.join(
+          tempFolder,
+          path.basename(url16).replace('.tar.gz', '.tar')
+        );
 
-      setCurrentStep(`Java16 - Extracting 2 / ${totalSteps}`);
-      const tempTarName16 = path.join(
-        tempFolder,
-        path.basename(url16).replace('.tar.gz', '.tar')
-      );
-
-      const secondExtractionJava16 = extractFull(tempTarName16, tempFolder, {
-        $bin: sevenZipPath,
-        $progress: true
-      });
-      await new Promise((resolve, reject) => {
-        secondExtractionJava16.on('progress', ({ percent }) => {
-          ipcRenderer.invoke('update-progress-bar', percent);
-          setDownloadPercentage(percent);
-          setCurrentStepPercentage(
-            27.5 + 27.5 + 10 + 10 + 10 + percent / (100 / 10)
-          );
+        const secondExtractionJava16 = extractFull(tempTarName16, tempFolder, {
+          $bin: sevenZipPath,
+          $progress: true
         });
-        secondExtractionJava16.on('end', () => {
-          resolve();
+        await new Promise((resolve, reject) => {
+          secondExtractionJava16.on('progress', ({ percent }) => {
+            ipcRenderer.invoke('update-progress-bar', percent);
+            setDownloadPercentage(percent);
+            setCurrentStepPercentage(
+              27.5 + 27.5 + 10 + 10 + 10 + percent / (100 / 10)
+            );
+          });
+          secondExtractionJava16.on('end', () => {
+            resolve();
+          });
+          secondExtractionJava16.on('error', err => {
+            reject(err);
+          });
         });
-        secondExtractionJava16.on('error', err => {
-          reject(err);
-        });
-      });
-      await fse.remove(tempTarName16);
+        await fse.remove(tempTarName16);
+      }
     }
 
     setCurrentStep('Cleanup');
