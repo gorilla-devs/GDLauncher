22ebb2cb1f1c465e6f94a1b1b5300356304f9e31
Code-Ac <76821666+Code-Ac@users.noreply.github.com>
2021-09-25 15:59:16 +0200

    7zFix - again

--- src/app/desktop/utils/index.js
+++ src/app/desktop/utils/index.js
@@ -321,7 +321,8 @@ export const isJavaPathOK = async (javaFolder, retry) => {
 export const get7zPath = async () => {
   // Get userData from ipc because we can't always get this from redux
   let baseDir = await ipcRenderer.invoke('getExecutablePath');
-  if (process.env.NODE_ENV === 'development') {
+  const isDev = process.env.NODE_ENV === 'development';
+  if (isDev) {
     baseDir = path.resolve(baseDir, '../../');
     if (process.platform === 'win32') {
       baseDir = path.join(baseDir, '7zip-bin/win/x64');
@@ -335,7 +336,7 @@ export const get7zPath = async () => {
     return path.join(baseDir, '7za');
   }
   if (process.platform === 'darwin') {
-    return path.resolve(baseDir, '../', '7za');
+    return path.resolve(baseDir, isDev ? '' : '../', '7za');
   }
   return path.join(baseDir, '7za.exe');
 };
