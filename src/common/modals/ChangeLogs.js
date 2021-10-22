d005195d657cb3119239d2c42772a817ed3c2b77
Code-Ac <76821666+Code-Ac@users.noreply.github.com>
2021-10-22 21:28:00 +0200

    Changelog

--- src/common/modals/ChangeLogs.js
+++ src/common/modals/ChangeLogs.js
@@ -68,96 +68,144 @@ const UpdateRow = ({ header, content, advanced }) => {
 const data = {
   new: [
     {
-      header: 'We now automatically take care of java16.',
+      header: 'Started rollout of custom APIs',
       content:
-        'You can now run Minecraft >1.17 without issues!. You can also individually select a manual path for both versions from the settings.'
+        'because CurseForge is planning to close their APIs in the future. ' +
+        'These custom APIs are disabled at the moment.',
+      advanced: { cm: '2b37e27a', ms: 'Noo CurseForge, why?' }
     },
     {
-      header: 'You can now easily duplicate instances.',
-      content: 'Just right-click on an instance and duplicate it.'
+      header: 'Moved from Patreon to Ko-fi',
+      content: 'as you can see above',
+      advanced: { cm: 'a95c6e1c' }
     },
     {
-      header: 'Added support for forge 1.17!',
-      content: "Let's hope they don't change their stuff again anytime soon 😬."
+      header: 'Added setting to use Symlinks',
+      content: 'instead of absolute paths to fix issues whit to long paths',
+      advanced: { cm: 'abe61b07' }
     },
     {
-      header: 'Added privacy policy, ToS and acceptable use policy!',
-      content:
-        "You can go read them from the settings page if you're into legal stuff."
+      header: 'Option to sort Instances',
+      content: 'by selecting the preferred order in the settings. :D',
+      advanced: { cm: 'dda24322', pr: '887' }
+    },
+    {
+      header: 'Added option to open the export folder',
+      content: 'after exporting an instance',
+      advanced: { cm: 'e15d6548', pr: '1082' }
     }
   ],
   improvements: [
     {
-      header: 'You can now select more java memory',
-      content: 'in the memory slider up to the amount available on your device.'
+      header: 'Added a "Check for updates" button',
+      content: 'to release CurseForge from a lot of automated API requests',
+      advanced: { cm: 'f12465f2' }
     },
     {
-      header: 'Improved the design of the changelog modal',
-      content: 'as you can clearly see from here 😃.'
+      header: 'Changed the buttons in the Mod Browser',
+      content: 'to good looking icons',
+      advanced: { cm: '399939a5', pr: '1035' }
     },
     {
-      header: 'Added social links to the settings sidebar',
-      content: ''
+      header: 'Updated Electron',
+      content: 'and murmur2 dependencies to their latest versions.',
+      advanced: { cm: 'f03d81b5', ms: 'Upgraded Electron to v15.1.0' }
     },
     {
-      header: 'Drastically improved performance',
-      content:
-        'for modal pages such as instances creator, instances manager and settings.'
-    },
-    {
-      header: 'Updated dependencies',
-      content: 'for security and performance improvements.'
-    },
-    {
-      header: 'Added usual MacOS default menu'
-    },
-    {
-      header: 'Improved Discord RPC.',
-      content:
-        "It now shows the modpack / MC version you're playing. The modpack name will only be shown for new instances."
-    },
-    {
-      header: 'Modal animation is now smoother and simpler.',
-      content: 'This should make it feel "faster".'
+      header: 'Batching multiple CurseForge requests together',
+      content: 'to increase request performance',
+      advanced: { cm: '422f9caf' }
     }
   ],
   bugfixes: [
     {
-      header: 'Fixed accounts being hidden',
-      content: 'when too many were added.'
+      header: 'Fixed 7zip not unzipping',
+      content: 'the downloaded files.',
+      advanced: {
+        cm: '2240004d',
+        ms:
+          'You want to know the true story? He did this, then I told him ' +
+          "that it doesn't work so he did it again and it partially worked. " +
+          'Then, third time, he stole my working code and committed it! :o'
+      }
     },
     {
-      header: 'Fixed concurrent download preference',
-      content: 'not being used when downloading FTB modpacks.'
+      header: 'Fixed MacOS .DS_Store files',
+      content: 'being detected as instances.',
+      advanced: { cm: '7528e587', pr: '1045/511af67' }
     },
     {
-      header: 'Fixed fabric mods not loading',
-      content: 'due to curseforge changing their backend structure.'
+      header: 'Fixed ModBrowser icons',
+      content: 'not being sized correctly.',
+      advanced: { cm: '7528e587', pr: '1045/9a35c56' }
     },
     {
-      header:
-        "Fixed a bug where we didn't correctly detect the curseforge modloader.",
-      content: ''
+      header: 'Corrected wrong spelled CSS code',
+      content: 'which was inherited anyways.',
+      advanced: { cm: '7528e587', pr: '1045' }
     },
     {
-      header: 'Fixed imports from external sources.',
-      content: 'Both local zips and remote urls should now work correctly.'
+      header: 'Fixed console error on Beta channel',
+      content: 'where release channel was undefined.',
+      advanced: { cm: '7528e587', pr: '1045/d185730' }
+    },
+    {
+      header: 'Fixed MacOS navbar logo',
+      content: 'being a bit to far on the right',
+      advanced: { cm: '5a245c99', pr: '1074' }
+    },
+    {
+      header: 'Fixed beta releases on Portable',
+      content: 'not downloading.',
+      advanced: { cm: 'e32ee91f' }
+    },
+    {
+      header: 'Hopefully fixed FTB downloads',
+      content: 'failing because of invalid file hashes',
+      advanced: {
+        cm: '77988a42',
+        ms:
+          "In reality I don't really know what davide did there, I just " +
+          'pretend to do so ;)'
+      }
+    },
+    {
+      header: 'Fixed mod file hash check',
+      advanced: { cm: '5fd0deb4' }
+    },
+    {
+      header: 'Fixed icons in .deb release',
+      content: 'not showing up.',
+      advanced: { cm: 'a7f1cb35', pr: '1039' }
+    },
+    {
+      header: 'Fixed startup crash',
+      content: 'only occurring in beta',
+      advanced: { cm: '1cff7e08' }
+    },
+    {
+      header: 'Fixed squashed Image',
+      content: 'in the screenshots',
+      advanced: { cm: '17e3f8b8', pr: '1074' }
     },
     {
       header: 'Fixed crash',
-      content: 'when renaming instances.'
+      content: 'when switching tabs too fast',
+      advanced: {
+        cm: '17e3f8b8',
+        pr: '1074',
+        ms: 'You will need a good mouse to experience that :)'
+      }
     },
     {
-      header: 'Fixed news',
-      content: 'not being correctly parsed sometimes.'
+      header: 'Fixed some wording and punctuation',
+      content: 'in the settings',
+      advanced: { cm: '248d8ae8', pr: '1080', ms: 'Good eye Huantian!' }
     },
     {
-      header: 'Fixed export',
-      content: 'not exporting correctly lol.'
-    },
-    {
-      header: 'Fixed crash',
-      content: 'when browsing mods.'
+      header: 'Disabled symlink option for non-Windows',
+      content: 'because it is not needed and not working there.',
+      advanced: { cm: '7d8289b2', pr: '1097' }
     }
   ]
 };
@@ -432,7 +480,7 @@ const Section = styled.div`
   p {
     margin: 20px 0 !important;
   }
-  div {
+  > div {
     width: 100%;
     margin: 20px 0;
     border-radius: 5px;
