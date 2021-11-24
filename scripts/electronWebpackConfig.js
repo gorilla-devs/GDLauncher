ec5371e4633d2ba2a97df9cfb633761adb1337ca
Davide <davide.ceschia@gmail.com>
2021-11-24 14:25:59 +0100

    test ga_id

--- scripts/electronWebpackConfig.js
+++ scripts/electronWebpackConfig.js
@@ -66,7 +66,7 @@ const baseConfig = {
     new webpack.EnvironmentPlugin({
       NODE_ENV: process.env.NODE_ENV,
       REACT_APP_RELEASE_TYPE: process.env.REACT_APP_RELEASE_TYPE,
-      GA_ID: process.env.GA_ID
+      REACT_APP_GA_ID: process.env.GA_ID
     }),
     new webpack.NamedModulesPlugin()
   ]
