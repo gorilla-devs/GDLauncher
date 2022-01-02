bccf316734fa5b639e75ea8f8cd7b023c2b9b34e
Davide <davide.ceschia@gmail.com>
2022-01-02 17:22:31 +0100

    test

--- packages/web/svelte.config.js
+++ packages/web/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +10,7 @@ const config = {
 		ssr: false,
 		hydrate: true,
 		prerender: {
-			enabled: true
+			enabled: false
 		}
 	}
 };
