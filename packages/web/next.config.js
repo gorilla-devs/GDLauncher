1f780fc5d373fdd9e82fca8cf13d3f5036a5bdb0
Davide <davide.ceschia@gmail.com>
2022-01-04 19:36:51 +0100

    chore: use preact instead of react

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
 };
