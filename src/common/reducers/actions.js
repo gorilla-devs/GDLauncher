b9a2016937b5bfdcb69ba48440a0076a559d1cb2
Gianmarco Cavallo <gianmarco.cavallo.consulente@bccsi.bcc.it>
2022-06-26 11:06:37 +0200

    fix: launch java process without shell only on win

--- src/common/reducers/actions.js
+++ src/common/reducers/actions.js
@@ -3123,7 +3123,7 @@ export function launchInstance(instanceName, forceQuit = false) {
       ),
       {
         cwd: instancePath,
-        shell: process.platform === 'win32'
+        shell: process.platform !== 'win32'
       }
     );
 
