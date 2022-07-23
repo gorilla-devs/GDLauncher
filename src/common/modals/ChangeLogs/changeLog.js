32f3d14afe3198220aecf7417d84b73c18bf5c05
Code-Ac <adrianstruwe@web.de>
2022-07-23 21:42:26 +0200

    v1.1.26

--- src/common/modals/ChangeLogs/changeLog.js
+++ src/common/modals/ChangeLogs/changeLog.js
@@ -1,11 +1,37 @@
 module.exports = {
-  new: [],
+  new: [
+    {
+      header: 'Squashed Git Commit history',
+      content: 'because it was around 0.5G big.',
+      advanced: { cm: '8ec97baa' }
+  ],
   improvements: [],
   bugfixes: [
     {
-      header: 'Fixed cloudflare blocking opted out mods during download.',
-      content: '',
-      advanced: { cm: '' }
-    }
+      header: 'Fixed the Memory slider',
+      content: 'reseting to a very low point.',
+      advanced: { cm: '8ec97baa' }
+    },
+    {
+      header: 'Fixed the Update all Mods Button',
+      content: 'sometimes not working properly.',
+      advanced: { cm: '1d17f128' }
+    },
+    {
+      header: 'Fixed Forge 1.19,
+      content: 'erroring with "launch string to long".',
+      advanced: { cm: '8ec97baa' }
+    },
+    {
+      header: 'Fixed the Open Mod page link',
+      content: 'not working.',
+      advanced: { cm: '8ec97baa' }
+    },
+    {
+      header: 'Fixed the X button in search bars',
+      content: 'not functioning.',
+      advanced: { cm: '8ec97baa' }
+    },
+
   ]
 };
