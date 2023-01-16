60b3fd0cb37126a147bd495393c608c52f7fc1c3
Celeo <mattboulanger@fastmail.com>
2023-01-15 21:15:37 -0800

    WIP

--- public/electron.js
+++ public/electron.js
@@ -592,7 +592,7 @@ ipcMain.handle('openMainBrowserTo', (e, urls) => {
     start = 'xdg-open';
   }
   for (const url of urls) {
-    promisify(exec)(`${start} ${url}`);
+    exec(`${start} ${url}`);
   }
 });
 
